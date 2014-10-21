7f3b2a9231c5f8a19cb0648d6d1604e15a6e7267
taeyoon0.lee <taeyoon0lee@taeyoon0lee-Samsung-DeskTop-System.(none)>
2014-10-21 09:27:20 +0900

    Core API compatibility   addition of "under_tizen_2_3_capi_enable" for version under tizen 2.3 core api
    Change-Id: Ic9b62d0abc047dd792513f8bba09d118d2d496a3

--- adaptors/tizen/tilt-sensor-impl-tizen.cpp
+++ adaptors/tizen/tilt-sensor-impl-tizen.cpp
@@ -20,11 +20,13 @@
 
 // EXTERNAL INCLUDES
 #include <cmath>
-#ifdef DALI_PROFILE_LITE
-#include <sensor_internal.h>
-#else
+
+#ifdef UNDER_TIZEN_2_3_CAPI
 #include <sensor.h>
+#else
+#include <sensor_internal.h>
 #endif
+
 
 #include <dali/public-api/object/type-registry.h>
 #include <dali/integration-api/debug.h>
