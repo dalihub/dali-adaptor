tizenorg/platform.core.uifw.dali-adaptor

Added public API to get native window handle from Window
Change-Id: Ib4aca5cd77d84710ce6d7f37f77104e6596ad555

adaptors/common/window-impl.h
@@ -161,6 +161,11 @@ public:
   Dali::DragAndDropDetector GetDragAndDropDetector() const;
 
   /**
+   * @copydoc Dali::Window::GetHandle() const
+   */
+  Dali::Any GetHandle() const;
+
+  /**
    * Called from Orientation after the Change signal has been sent
    */
   void RotationDone( int orientation, int width, int height );

adaptors/public-api/adaptor-framework/window.cpp
@@ -131,6 +131,11 @@ DragAndDropDetector Window::GetDragAndDropDetector() const
   return GetImplementation(*this).GetDragAndDropDetector();
 }
 
+Any Window::GetHandle() const
+{
+  return GetImplementation(*this).GetHandle();
+}
+
 Window::Window( Internal::Adaptor::Window* window )
 : BaseHandle( window )
 {

adaptors/wayland/window-impl-wl.cpp
@@ -358,6 +358,18 @@ Dali::DragAndDropDetector Window::GetDragAndDropDetector() const
   return mDragAndDropDetector;
 }
 
+Dali::Any Window::GetHandle() const
+{
+  if(mEventHandler)
+  {
+    return mEventHandler->mEcoreWindow;
+  }
+  else
+  {
+    return Dali::Any();
+  }
+}
+
 void Window::OnStart()
 {
   DoShowIndicator( mIndicatorOrientation );

adaptors/x11/window-impl-x.cpp
@@ -553,6 +553,18 @@ Dali::DragAndDropDetector Window::GetDragAndDropDetector() const
   return mDragAndDropDetector;
 }
 
+Dali::Any Window::GetHandle() const
+{
+  if(mEventHandler)
+  {
+    return mEventHandler->mEcoreWindow;
+  }
+  else
+  {
+    return Dali::Any();
+  }
+}
+
 void Window::OnStart()
 {
   DoShowIndicator( mIndicatorOrientation );

dalihub/dali-adaptor

adaptors/common/window-impl.h
@@ -161,6 +161,11 @@ public:
   Dali::DragAndDropDetector GetDragAndDropDetector() const;
 
   /**
+   * @copydoc Dali::Window::GetNativeHandle() const
+   */
+  Dali::Any GetNativeHandle() const;
+
+  /**
    * Called from Orientation after the Change signal has been sent
    */
   void RotationDone( int orientation, int width, int height );

adaptors/public-api/adaptor-framework/window.cpp
@@ -131,6 +131,11 @@ DragAndDropDetector Window::GetDragAndDropDetector() const
   return GetImplementation(*this).GetDragAndDropDetector();
 }
 
+Any Window::GetNativeHandle() const
+{
+  return GetImplementation(*this).GetNativeHandle();
+}
+
 Window::Window( Internal::Adaptor::Window* window )
 : BaseHandle( window )
 {

adaptors/wayland/window-impl-wl.cpp
@@ -358,6 +358,18 @@ Dali::DragAndDropDetector Window::GetDragAndDropDetector() const
   return mDragAndDropDetector;
 }
 
+Dali::Any Window::GetNativeHandle() const
+{
+  if(mEventHandler)
+  {
+    return mEventHandler->mEcoreWindow;
+  }
+  else
+  {
+    return Dali::Any();
+  }
+}
+
 void Window::OnStart()
 {
   DoShowIndicator( mIndicatorOrientation );

adaptors/x11/window-impl-x.cpp
@@ -553,6 +553,18 @@ Dali::DragAndDropDetector Window::GetDragAndDropDetector() const
   return mDragAndDropDetector;
 }
 
+Dali::Any Window::GetNativeHandle() const
+{
+  if(mEventHandler)
+  {
+    return mEventHandler->mEcoreWindow;
+  }
+  else
+  {
+    return Dali::Any();
+  }
+}
+
 void Window::OnStart()
 {
   DoShowIndicator( mIndicatorOrientation );