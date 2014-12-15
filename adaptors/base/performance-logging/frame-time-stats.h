tizenorg/platform.core.uifw.dali-adaptor

Expose custom performance timers
Change-Id: I4e11fc2a4d82a6a2b5ea3366067ff033a76ff219

adaptors/base/performance-logging/frame-time-stats.h
@@ -22,6 +22,8 @@
 
 #include "frame-time-stamp.h"
 
+#include <vector>
+
 namespace Dali
 {
 
@@ -66,11 +68,6 @@ struct FrameTimeStats
    void Reset();
 
    /**
-    * @return rolling average time in seconds
-    */
-   float GetRollingAverageTime() const;
-
-   /**
     * @return maximum time in seconds
     */
    float GetMaxTime() const;
@@ -90,6 +87,14 @@ struct FrameTimeStats
     */
    unsigned int GetRunCount() const;
 
+   /**
+    * Calculate the mean and standard deviation
+    *
+    * @param[out] mean The return mean value
+    * @param[out] standardDeviation The return standard deviation value
+    */
+   void CalculateMean( float& meanOut, float& standardDeviationOut ) const;
+
 private:
 
    /**
@@ -101,9 +106,11 @@ private:
      WAITING_FOR_END_TIME       ///< waiting for end time marker
    };
 
+   typedef std::vector< unsigned int > Samples;
+   Samples mSamples;
+
    unsigned int mMin;                  ///< current minimum value in microseconds
    unsigned int mMax;                  ///< current maximum value in microseconds
-   unsigned int mAvg;                  ///< current average in microseconds
    unsigned int mTotal;                ///< current total in in microseconds
    unsigned int mRunCount;      ///< how many times the timer has been start / stopped
    FrameTimeStamp mStart;       ///< start time stamp, to calculate the diff

adaptors/base/update-render-synchronization.cpp
@@ -94,7 +94,7 @@ void UpdateRenderSynchronization::Pause()
 {
   mPaused = true;
 
-  AddPerformanceMarker( PerformanceMarker::PAUSED );
+  AddPerformanceMarker( PerformanceInterface::PAUSED );
   mFrameTime.Suspend();
 }
 
@@ -111,7 +111,7 @@ void UpdateRenderSynchronization::Resume()
   mPausedCondition.notify_one();
   mVSyncSleepCondition.notify_one();
 
-  AddPerformanceMarker( PerformanceMarker::RESUME);
+  AddPerformanceMarker( PerformanceInterface::RESUME);
 }
 
 void UpdateRenderSynchronization::UpdateRequested()
@@ -184,13 +184,13 @@ void UpdateRenderSynchronization::UpdateReadyToRun()
     WaitSync();
   }
 
-  AddPerformanceMarker( PerformanceMarker::UPDATE_START );
+  AddPerformanceMarker( PerformanceInterface::UPDATE_START );
 }
 
 bool UpdateRenderSynchronization::UpdateSyncWithRender( bool& renderNeedsUpdate )
 {
 
-  AddPerformanceMarker( PerformanceMarker::UPDATE_END );
+  AddPerformanceMarker( PerformanceInterface::UPDATE_END );
 
   boost::unique_lock< boost::mutex > lock( mMutex );
 
@@ -285,7 +285,7 @@ bool UpdateRenderSynchronization::RenderSyncWithUpdate(RenderRequest*& requestPt
 
   if( mRunning )
   {
-    AddPerformanceMarker( PerformanceMarker::RENDER_START );
+    AddPerformanceMarker( PerformanceInterface::RENDER_START );
   }
 
   // write any new requests
@@ -321,7 +321,7 @@ void UpdateRenderSynchronization::RenderFinished( bool updateRequired, bool requ
     mRequestFinishedCondition.notify_one();
   }
 
-  AddPerformanceMarker( PerformanceMarker::RENDER_END );
+  AddPerformanceMarker( PerformanceInterface::RENDER_END );
 }
 
 void UpdateRenderSynchronization::WaitSync()
@@ -367,7 +367,7 @@ bool UpdateRenderSynchronization::VSyncNotifierSyncWithUpdateAndRender( bool val
 
   mVSyncReceivedCondition.notify_all();
 
-  AddPerformanceMarker( PerformanceMarker::V_SYNC );
+  AddPerformanceMarker( PerformanceInterface::V_SYNC );
 
   while( mRunning && // sleep on condition variable WHILE still running
          !mAllowUpdateWhilePaused &&             // AND NOT allowing updates while paused
@@ -406,7 +406,7 @@ void UpdateRenderSynchronization::SetRenderRefreshRate( unsigned int numberOfVSy
   mNumberOfVSyncsPerRender = numberOfVSyncsPerRender;
 }
 
-inline void UpdateRenderSynchronization::AddPerformanceMarker( PerformanceMarker::MarkerType type )
+inline void UpdateRenderSynchronization::AddPerformanceMarker( PerformanceInterface::MarkerType type )
 {
   if( mPerformanceInterface )
   {

dalihub/dali-adaptor

adaptors/base/performance-logging/frame-time-stats.h
@@ -19,8 +19,9 @@
  */
 
 // INTERNAL INCLUDES
+#include <base/performance-logging/frame-time-stamp.h>
+#include <dali/public-api/common/dali-vector.h>
 
-#include "frame-time-stamp.h"
 
 namespace Dali
 {
@@ -66,11 +67,6 @@ struct FrameTimeStats
    void Reset();
 
    /**
-    * @return rolling average time in seconds
-    */
-   float GetRollingAverageTime() const;
-
-   /**
     * @return maximum time in seconds
     */
    float GetMaxTime() const;
@@ -90,6 +86,14 @@ struct FrameTimeStats
     */
    unsigned int GetRunCount() const;
 
+   /**
+    * Calculate the mean and standard deviation
+    *
+    * @param[out] mean The return mean value
+    * @param[out] standardDeviation The return standard deviation value
+    */
+   void CalculateMean( float& meanOut, float& standardDeviationOut ) const;
+
 private:
 
    /**
@@ -101,9 +105,11 @@ private:
      WAITING_FOR_END_TIME       ///< waiting for end time marker
    };
 
+   typedef Dali::Vector< unsigned int > Samples;
+   Samples mSamples;
+
    unsigned int mMin;                  ///< current minimum value in microseconds
    unsigned int mMax;                  ///< current maximum value in microseconds
-   unsigned int mAvg;                  ///< current average in microseconds
    unsigned int mTotal;                ///< current total in in microseconds
    unsigned int mRunCount;      ///< how many times the timer has been start / stopped
    FrameTimeStamp mStart;       ///< start time stamp, to calculate the diff

adaptors/base/update-render-synchronization.cpp
@@ -94,7 +94,7 @@ void UpdateRenderSynchronization::Pause()
 {
   mPaused = true;
 
-  AddPerformanceMarker( PerformanceMarker::PAUSED );
+  AddPerformanceMarker( PerformanceInterface::PAUSED );
   mFrameTime.Suspend();
 }
 
@@ -111,7 +111,7 @@ void UpdateRenderSynchronization::Resume()
   mPausedCondition.notify_one();
   mVSyncSleepCondition.notify_one();
 
-  AddPerformanceMarker( PerformanceMarker::RESUME);
+  AddPerformanceMarker( PerformanceInterface::RESUME);
 }
 
 void UpdateRenderSynchronization::UpdateRequested()
@@ -184,13 +184,13 @@ void UpdateRenderSynchronization::UpdateReadyToRun()
     WaitSync();
   }
 
-  AddPerformanceMarker( PerformanceMarker::UPDATE_START );
+  AddPerformanceMarker( PerformanceInterface::UPDATE_START );
 }
 
 bool UpdateRenderSynchronization::UpdateSyncWithRender( bool& renderNeedsUpdate )
 {
 
-  AddPerformanceMarker( PerformanceMarker::UPDATE_END );
+  AddPerformanceMarker( PerformanceInterface::UPDATE_END );
 
   boost::unique_lock< boost::mutex > lock( mMutex );
 
@@ -285,7 +285,7 @@ bool UpdateRenderSynchronization::RenderSyncWithUpdate(RenderRequest*& requestPt
 
   if( mRunning )
   {
-    AddPerformanceMarker( PerformanceMarker::RENDER_START );
+    AddPerformanceMarker( PerformanceInterface::RENDER_START );
   }
 
   // write any new requests
@@ -321,7 +321,7 @@ void UpdateRenderSynchronization::RenderFinished( bool updateRequired, bool requ
     mRequestFinishedCondition.notify_one();
   }
 
-  AddPerformanceMarker( PerformanceMarker::RENDER_END );
+  AddPerformanceMarker( PerformanceInterface::RENDER_END );
 }
 
 void UpdateRenderSynchronization::WaitSync()
@@ -367,7 +367,7 @@ bool UpdateRenderSynchronization::VSyncNotifierSyncWithUpdateAndRender( bool val
 
   mVSyncReceivedCondition.notify_all();
 
-  AddPerformanceMarker( PerformanceMarker::V_SYNC );
+  AddPerformanceMarker( PerformanceInterface::VSYNC );
 
   while( mRunning && // sleep on condition variable WHILE still running
          !mAllowUpdateWhilePaused &&             // AND NOT allowing updates while paused
@@ -406,7 +406,7 @@ void UpdateRenderSynchronization::SetRenderRefreshRate( unsigned int numberOfVSy
   mNumberOfVSyncsPerRender = numberOfVSyncsPerRender;
 }
 
-inline void UpdateRenderSynchronization::AddPerformanceMarker( PerformanceMarker::MarkerType type )
+inline void UpdateRenderSynchronization::AddPerformanceMarker( PerformanceInterface::MarkerType type )
 {
   if( mPerformanceInterface )
   {