tizenorg/platform.core.uifw.dali-adaptor

add IndicatorVisibilityChangedSignal
Change-Id: Ib6279516bac7b1215f7576ea472100757ad9fcfc

adaptors/common/indicator-impl.cpp
@@ -1305,6 +1305,11 @@ void Indicator::OnAnimationFinished(Dali::Animation& animation)
   if( !mIsShowing )
   {
     mIndicatorActor.SetVisible( false );
+
+    if( mObserver != NULL )
+    {
+      mObserver->IndicatorVisibleChanged( mIsShowing ); // is showing?
+    }
   }
 }
 

adaptors/common/indicator-impl.h
@@ -84,6 +84,12 @@ public:
      * @param[in] indicator The indicator that has finished uploading.
      */
     virtual void IndicatorClosed(Indicator* indicator) = 0;
+
+    /**
+     * Notify the observer when the indicator visible status is changed.
+     * @param[in] isShowing Whether the indicator is visible.
+     */
+    virtual void IndicatorVisibleChanged( bool isVisible ) = 0;
   };
 
 protected:

adaptors/common/window-impl.h
@@ -59,6 +59,8 @@ typedef IntrusivePtr<Orientation> OrientationPtr;
 class Window : public Dali::BaseObject, public Indicator::Observer, public LifeCycleObserver
 {
 public:
+  typedef Dali::Window::IndicatorSignalV2 IndicatorSignalV2;
+
   /**
    * Create a new Window. This should only be called once by the Application class
    * @param[in] windowPosition The position and size of the window
@@ -222,6 +224,11 @@ private: // Indicator::Observer interface
    */
   virtual void IndicatorClosed(Indicator* indicator);
 
+  /**
+   * @copydoc Dali::Internal::Adaptor::Indicator::Observer::IndicatorVisibleChanged()
+   */
+  virtual void IndicatorVisibleChanged( bool isVisible );
+
 private: // Adaptor::Observer interface
 
   /**
@@ -249,6 +256,12 @@ private: // Adaptor::Observer interface
    */
   virtual void OnDestroy();
 
+public: // Signals
+  /**
+   * The user should connect to this signal to get a timing when indicator was shown / hidden.
+   */
+  IndicatorSignalV2& IndicatorVisibilityChangedSignal() { return mIndicatorVisibilityChangedSignalV2; }
+
 private:
 
   typedef std::vector<Indicator*> DiscardedIndicators;
@@ -275,6 +288,9 @@ private:
   OrientationPtr                               mOrientation;
   std::vector<Dali::Window::WindowOrientation> mAvailableOrientations;
   Dali::Window::WindowOrientation              mPreferredOrientation;
+
+  // Signals
+  IndicatorSignalV2 mIndicatorVisibilityChangedSignalV2;
 };
 
 } // namespace Adaptor

adaptors/wayland/window-impl-wl.cpp
@@ -310,6 +310,11 @@ void Window::IndicatorClosed( Indicator* indicator )
   }
 }
 
+void Window::IndicatorVisibleChanged(bool isVisible)
+{
+  mIndicatorVisibilityChangedSignalV2.Emit(isVisible);
+}
+
 void Window::SetIndicatorActorRotation()
 {
   DALI_LOG_TRACE_METHOD( gWindowLogFilter );

adaptors/x11/window-impl-x.cpp
@@ -487,6 +487,11 @@ void Window::IndicatorClosed( Indicator* indicator )
   }
 }
 
+void Window::IndicatorVisibleChanged(bool isVisible)
+{
+  mIndicatorVisibilityChangedSignalV2.Emit(isVisible);
+}
+
 void Window::SetIndicatorActorRotation()
 {
   DALI_LOG_TRACE_METHOD( gWindowLogFilter );

dalihub/dali-adaptor

Change-Id: I3e7dff4c6775cd57de0c47a51470b5ef77798f0a

adaptors/common/indicator-impl.cpp
@@ -1305,6 +1305,11 @@ void Indicator::OnAnimationFinished(Dali::Animation& animation)
   if( !mIsShowing )
   {
     mIndicatorActor.SetVisible( false );
+
+    if( mObserver != NULL )
+    {
+      mObserver->IndicatorVisibilityChanged( mIsShowing ); // is showing?
+    }
   }
 }
 

adaptors/common/indicator-impl.h
@@ -84,6 +84,12 @@ public:
      * @param[in] indicator The indicator that has finished uploading.
      */
     virtual void IndicatorClosed(Indicator* indicator) = 0;
+
+    /**
+     * Notify the observer when the indicator visible status is changed.
+     * @param[in] isShowing Whether the indicator is visible.
+     */
+    virtual void IndicatorVisibilityChanged( bool isVisible ) = 0;
   };
 
 protected:

adaptors/common/window-impl.h
@@ -59,6 +59,8 @@ typedef IntrusivePtr<Orientation> OrientationPtr;
 class Window : public Dali::BaseObject, public Indicator::Observer, public LifeCycleObserver
 {
 public:
+  typedef Dali::Window::IndicatorSignalV2 IndicatorSignalV2;
+
   /**
    * Create a new Window. This should only be called once by the Application class
    * @param[in] windowPosition The position and size of the window
@@ -222,6 +224,11 @@ private: // Indicator::Observer interface
    */
   virtual void IndicatorClosed(Indicator* indicator);
 
+  /**
+   * @copydoc Dali::Internal::Adaptor::Indicator::Observer::IndicatorVisibilityChanged()
+   */
+  virtual void IndicatorVisibilityChanged( bool isVisible );
+
 private: // Adaptor::Observer interface
 
   /**
@@ -249,6 +256,12 @@ private: // Adaptor::Observer interface
    */
   virtual void OnDestroy();
 
+public: // Signals
+  /**
+   * The user should connect to this signal to get a timing when indicator was shown / hidden.
+   */
+  IndicatorSignalV2& IndicatorVisibilityChangedSignal() { return mIndicatorVisibilityChangedSignalV2; }
+
 private:
 
   typedef std::vector<Indicator*> DiscardedIndicators;
@@ -275,6 +288,9 @@ private:
   OrientationPtr                               mOrientation;
   std::vector<Dali::Window::WindowOrientation> mAvailableOrientations;
   Dali::Window::WindowOrientation              mPreferredOrientation;
+
+  // Signals
+  IndicatorSignalV2 mIndicatorVisibilityChangedSignalV2;
 };
 
 } // namespace Adaptor

adaptors/wayland/window-impl-wl.cpp
@@ -310,6 +310,11 @@ void Window::IndicatorClosed( Indicator* indicator )
   }
 }
 
+void Window::IndicatorVisibilityChanged(bool isVisible)
+{
+  mIndicatorVisibilityChangedSignalV2.Emit(isVisible);
+}
+
 void Window::SetIndicatorActorRotation()
 {
   DALI_LOG_TRACE_METHOD( gWindowLogFilter );

adaptors/x11/window-impl-x.cpp
@@ -487,6 +487,11 @@ void Window::IndicatorClosed( Indicator* indicator )
   }
 }
 
+void Window::IndicatorVisibilityChanged(bool isVisible)
+{
+  mIndicatorVisibilityChangedSignalV2.Emit(isVisible);
+}
+
 void Window::SetIndicatorActorRotation()
 {
   DALI_LOG_TRACE_METHOD( gWindowLogFilter );