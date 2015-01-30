tizenorg/platform.core.uifw.dali-adaptor

DALi signals refactor to remove V2 naming
Change-Id: I675f19a97276f7dbda1d6f8569f6d3b49b9eb297

adaptors/common/sound-player-impl.h
@@ -43,7 +43,7 @@ class SoundPlayer : public Dali::BaseObject
 
 public:
 
-  typedef Dali::SoundPlayer::SoundPlayFinishedSignalV2 SoundPlayFinishedSignalV2;
+  typedef Dali::SoundPlayer::SoundPlayFinishedSignalType SoundPlayFinishedSignalType;
 
   /**
    * Create a SoundPlayer.
@@ -70,7 +70,7 @@ public:
   /**
    * @copydoc Dali::SoundPlayer::SoundPlayFinishedSignal()
    */
-  SoundPlayFinishedSignalV2& SoundPlayFinishedSignal();
+  SoundPlayFinishedSignalType& SoundPlayFinishedSignal();
 
   /**
    * Connects a callback function with the object's signals.
@@ -110,7 +110,7 @@ private:
 private:
 
   FeedbackPluginProxy mPlugin;
-  SoundPlayFinishedSignalV2 mSoundPlayFinishedSignalV2;
+  SoundPlayFinishedSignalType mSoundPlayFinishedSignalType;
 };
 
 } // namespace Adaptor

adaptors/tizen/tilt-sensor-impl.h
@@ -42,7 +42,7 @@ class TiltSensor : public Dali::BaseObject
 {
 public:
 
-  typedef Dali::TiltSensor::TiltedSignalV2 TiltedSignalV2;
+  typedef Dali::TiltSensor::TiltedSignalType TiltedSignalType;
 
   /**
    * Create a TiltSensor.
@@ -89,7 +89,7 @@ public:
   /**
    * @copydoc Dali::TiltSensor::TiltedSignal()
    */
-  TiltedSignalV2& TiltedSignal();
+  TiltedSignalType& TiltedSignal();
 
   /**
    * @copydoc Dali::TiltSensor::SetUpdateFrequency()
@@ -162,7 +162,7 @@ private:
   std::deque<float> mRollValues;
   std::deque<float> mPitchValues;
 
-  TiltedSignalV2 mTiltedSignalV2;
+  TiltedSignalType mTiltedSignalType;
 };
 
 } // namespace Adaptor

adaptors/wayland/imf-manager-impl.h
@@ -44,8 +44,8 @@ class RenderSurface;
 class ImfManager : public Dali::BaseObject
 {
 public:
-  typedef Dali::ImfManager::ImfManagerSignalV2 ImfManagerSignalV2;
-  typedef Dali::ImfManager::ImfEventSignalV2 ImfEventSignalV2;
+  typedef Dali::ImfManager::ImfManagerSignalType ImfManagerSignalType;
+  typedef Dali::ImfManager::ImfEventSignalType ImfEventSignalType;
 
 public:
 
@@ -156,12 +156,12 @@ public:  // Signals
   /**
    * @copydoc Dali::ImfManager::ActivatedSignal()
    */
-  ImfManagerSignalV2& ActivatedSignal() { return mActivatedSignalV2; }
+  ImfManagerSignalType& ActivatedSignal() { return mActivatedSignalType; }
 
   /**
    * @copydoc Dali::ImfManager::EventReceivedSignal()
    */
-  ImfEventSignalV2& EventReceivedSignal() { return mEventSignalV2; }
+  ImfEventSignalType& EventReceivedSignal() { return mEventSignalType; }
 
 protected:
 
@@ -191,8 +191,8 @@ private:
 
   std::vector<Dali::Integration::KeyEvent> mKeyEvents; ///< Stores key events to be sent from idle call-back.
 
-  ImfManagerSignalV2      mActivatedSignalV2;
-  ImfEventSignalV2        mEventSignalV2;
+  ImfManagerSignalType      mActivatedSignalType;
+  ImfEventSignalType        mEventSignalType;
 
 public:
 

adaptors/wayland/window-impl-wl.cpp
@@ -312,7 +312,7 @@ void Window::IndicatorClosed( Indicator* indicator )
 
 void Window::IndicatorVisibilityChanged(bool isVisible)
 {
-  mIndicatorVisibilityChangedSignalV2.Emit(isVisible);
+  mIndicatorVisibilityChangedSignalType.Emit(isVisible);
 }
 
 void Window::SetIndicatorActorRotation()

dalihub/dali-adaptor

adaptors/common/sound-player-impl.h
@@ -43,7 +43,7 @@ class SoundPlayer : public Dali::BaseObject
 
 public:
 
-  typedef Dali::SoundPlayer::SoundPlayFinishedSignalV2 SoundPlayFinishedSignalV2;
+  typedef Dali::SoundPlayer::SoundPlayFinishedSignalType SoundPlayFinishedSignalType;
 
   /**
    * Create a SoundPlayer.
@@ -70,7 +70,7 @@ public:
   /**
    * @copydoc Dali::SoundPlayer::SoundPlayFinishedSignal()
    */
-  SoundPlayFinishedSignalV2& SoundPlayFinishedSignal();
+  SoundPlayFinishedSignalType& SoundPlayFinishedSignal();
 
   /**
    * Connects a callback function with the object's signals.
@@ -110,7 +110,7 @@ private:
 private:
 
   FeedbackPluginProxy mPlugin;
-  SoundPlayFinishedSignalV2 mSoundPlayFinishedSignalV2;
+  SoundPlayFinishedSignalType mSoundPlayFinishedSignal;
 };
 
 } // namespace Adaptor

adaptors/tizen/tilt-sensor-impl.h
@@ -42,7 +42,7 @@ class TiltSensor : public Dali::BaseObject
 {
 public:
 
-  typedef Dali::TiltSensor::TiltedSignalV2 TiltedSignalV2;
+  typedef Dali::TiltSensor::TiltedSignalType TiltedSignalType;
 
   /**
    * Create a TiltSensor.
@@ -89,7 +89,7 @@ public:
   /**
    * @copydoc Dali::TiltSensor::TiltedSignal()
    */
-  TiltedSignalV2& TiltedSignal();
+  TiltedSignalType& TiltedSignal();
 
   /**
    * @copydoc Dali::TiltSensor::SetUpdateFrequency()
@@ -162,7 +162,7 @@ private:
   std::deque<float> mRollValues;
   std::deque<float> mPitchValues;
 
-  TiltedSignalV2 mTiltedSignalV2;
+  TiltedSignalType mTiltedSignal;
 };
 
 } // namespace Adaptor

adaptors/wayland/imf-manager-impl.h
@@ -44,8 +44,8 @@ class RenderSurface;
 class ImfManager : public Dali::BaseObject
 {
 public:
-  typedef Dali::ImfManager::ImfManagerSignalV2 ImfManagerSignalV2;
-  typedef Dali::ImfManager::ImfEventSignalV2 ImfEventSignalV2;
+  typedef Dali::ImfManager::ImfManagerSignalType ImfManagerSignalType;
+  typedef Dali::ImfManager::ImfEventSignalType ImfEventSignalType;
 
 public:
 
@@ -156,12 +156,12 @@ public:  // Signals
   /**
    * @copydoc Dali::ImfManager::ActivatedSignal()
    */
-  ImfManagerSignalV2& ActivatedSignal() { return mActivatedSignalV2; }
+  ImfManagerSignalType& ActivatedSignal() { return mActivatedSignal; }
 
   /**
    * @copydoc Dali::ImfManager::EventReceivedSignal()
    */
-  ImfEventSignalV2& EventReceivedSignal() { return mEventSignalV2; }
+  ImfEventSignalType& EventReceivedSignal() { return mEventSignal; }
 
 protected:
 
@@ -191,8 +191,8 @@ private:
 
   std::vector<Dali::Integration::KeyEvent> mKeyEvents; ///< Stores key events to be sent from idle call-back.
 
-  ImfManagerSignalV2      mActivatedSignalV2;
-  ImfEventSignalV2        mEventSignalV2;
+  ImfManagerSignalType      mActivatedSignal;
+  ImfEventSignalType        mEventSignal;
 
 public:
 

adaptors/wayland/window-impl-wl.cpp
@@ -312,7 +312,7 @@ void Window::IndicatorClosed( Indicator* indicator )
 
 void Window::IndicatorVisibilityChanged(bool isVisible)
 {
-  mIndicatorVisibilityChangedSignalV2.Emit(isVisible);
+  mIndicatorVisibilityChangedSignal.Emit(isVisible);
 }
 
 void Window::SetIndicatorActorRotation()