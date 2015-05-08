6bd81d2722a09836d3ea0fad6acb5ab3bf2764ab
Nick Holland <nick.holland@partner.samsung.com>
2015-05-08 13:52:26 +0100

    DALi Version 1.0.41
    Change-Id: I676db1ef215d9cd582aeefd881e10573462f1edc

--- adaptors/public-api/dali-adaptor-version.cpp
+++ adaptors/public-api/dali-adaptor-version.cpp
@@ -28,7 +28,7 @@ namespace Dali
 
 const unsigned int ADAPTOR_MAJOR_VERSION = 1;
 const unsigned int ADAPTOR_MINOR_VERSION = 0;
-const unsigned int ADAPTOR_MICRO_VERSION = 40;
+const unsigned int ADAPTOR_MICRO_VERSION = 41;
 const char * const ADAPTOR_BUILD_DATE    = __DATE__ " " __TIME__;
 
 #ifdef DEBUG_ENABLED
