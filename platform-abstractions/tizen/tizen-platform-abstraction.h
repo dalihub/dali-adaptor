tizenorg/platform.core.uifw.dali-adaptor

Shader binary synchronous simplified IO
Change-Id: I4894729a48527bcaa7b710d616eeeacdc7f7c8b3
Signed-off-by: Andrew Cox <andrew.cox@partner.samsung.com>

platform-abstractions/tizen/tizen-platform-abstraction.h
@@ -144,7 +144,7 @@ public: // PlatformAbstraction overrides
   /**
    * @copydoc PlatformAbstraction::LoadFile()
    */
-  virtual bool LoadFile( const std::string& filename, std::vector< unsigned char >& buffer ) const;
+  virtual bool LoadFile( const std::string& filename, Dali::Vector< unsigned char >& buffer ) const;
 
   /**
    * @copydoc PlatformAbstraction::LoadFile()
@@ -154,7 +154,7 @@ public: // PlatformAbstraction overrides
   /**
    * @copydoc PlatformAbstraction::SaveFile()
    */
-  virtual bool SaveFile(const std::string& filename, std::vector< unsigned char >& buffer) const;
+  virtual bool SaveFile(const std::string& filename, const unsigned char * buffer, unsigned int numBytes ) const;
 
   /**
    * @copydoc PlatformAbstraction::GetDynamicsFactory();
@@ -164,7 +164,12 @@ public: // PlatformAbstraction overrides
   /**
    * @copydoc PlatformAbstraction::LoadShaderBinFile()
    */
-  virtual bool LoadShaderBinFile( const std::string& filename, std::vector< unsigned char >& buffer ) const;
+  virtual bool LoadShaderBinFile( const std::string& filename, Dali::Vector< unsigned char >& buffer ) const;
+
+  /**
+   * @copydoc PlatformAbstraction::SaveShaderBinFile()
+   */
+  virtual bool SaveShaderBinFile( const std::string& filename, const unsigned char * buffer, unsigned int numBytes ) const;
 
   /**
    * Sets path for data/resource storage.

dalihub/dali-adaptor

platform-abstractions/tizen/tizen-platform-abstraction.h
@@ -143,7 +143,7 @@ public: // PlatformAbstraction overrides
   /**
    * @copydoc PlatformAbstraction::LoadFile()
    */
-  virtual bool LoadFile( const std::string& filename, std::vector< unsigned char >& buffer ) const;
+  virtual bool LoadFile( const std::string& filename, Dali::Vector< unsigned char >& buffer ) const;
 
   /**
    * @copydoc PlatformAbstraction::LoadFile()
@@ -153,12 +153,17 @@ public: // PlatformAbstraction overrides
   /**
    * @copydoc PlatformAbstraction::SaveFile()
    */
-  virtual bool SaveFile(const std::string& filename, std::vector< unsigned char >& buffer) const;
+  virtual bool SaveFile(const std::string& filename, const unsigned char * buffer, unsigned int numBytes ) const;
 
   /**
-   * @copydoc PlatformAbstraction::LoadShaderBinFile()
+   * @copydoc PlatformAbstraction::LoadShaderBinaryFile()
    */
-  virtual bool LoadShaderBinFile( const std::string& filename, std::vector< unsigned char >& buffer ) const;
+  virtual bool LoadShaderBinaryFile( const std::string& filename, Dali::Vector< unsigned char >& buffer ) const;
+
+  /**
+   * @copydoc PlatformAbstraction::SaveShaderBinaryFile()
+   */
+  virtual bool SaveShaderBinaryFile( const std::string& filename, const unsigned char * buffer, unsigned int numBytes ) const;
 
   /**
    * Sets path for data/resource storage.