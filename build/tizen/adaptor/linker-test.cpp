tizenorg/platform.core.uifw.dali-adaptor

Fix prevent issue - uncaught exception
In function main(int, char **) an exception of type Dali::DaliException is thrown and never caught.
MainLoop() may throw an exception.

Change-Id: Iaa61013fd07a287e360fb37d0a3548c7ea33343c

build/tizen/adaptor/linker-test.cpp
@@ -22,6 +22,7 @@
 #include <render-surface.h>
 #include <orientation.h>
 #include <timer.h>
+#include <iostream>
 
 using namespace Dali;
 
@@ -46,12 +47,26 @@ public:
 
 /*****************************************************************************/
 
+void RunTest( Application& application )
+{
+  LinkerApp linkerApp (application);
+
+  try
+  {
+    application.MainLoop();
+  }
+  catch(...)
+  {
+    std::cout << "Exception caught";
+  }
+}
+
 int
 main(int argc, char **argv)
 {
   Application app = Application::New(&argc, &argv);
-  LinkerApp linkerApp (app);
-  app.MainLoop();
+
+  RunTest( app );
 
   return 0;
 }

dalihub/dali-adaptor

build/tizen/adaptor/linker-test.cpp
@@ -22,6 +22,7 @@
 #include <render-surface.h>
 #include <orientation.h>
 #include <timer.h>
+#include <iostream>
 
 using namespace Dali;
 
@@ -50,8 +51,16 @@ int
 main(int argc, char **argv)
 {
   Application app = Application::New(&argc, &argv);
-  LinkerApp linkerApp (app);
-  app.MainLoop();
+
+  try
+  {
+    LinkerApp linkerApp (app);
+    app.MainLoop();
+  }
+  catch(...)
+  {
+    std::cout << "Exception caught";
+  }
 
   return 0;
 }