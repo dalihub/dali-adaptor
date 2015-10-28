37d9ad8911ddb2ee574ba2568f223d73efcda882
Xiangyin Ma <x1.ma@samsung.com>
2015-10-28 17:29:55 +0000

    Added API for getting environment variable
    Change-Id: I8d3bbc2aab18dc0c47aaca938d25c31e0a635070

---
+++ adaptors/devel-api/adaptor-framework/environment-variable.h
@@ -0,0 +1,40 @@
+#ifndef __DALI_ENVIRONMENT_VARIABLE_H__
+#define __DALI_ENVIRONMENT_VARIABLE_H__
+
+/*
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <dali/public-api/common/dali-common.h>
+
+namespace Dali
+{
+
+namespace EnvironmentVariable
+{
+
+/**
+ * @brief Search the environment list for the specified variable name and return a pointer to the C string that is associated with the matched environment list member.
+ *
+ * @param[in] variable Null-terminated character string identifying the name of the environmental variable to look for.
+ * @return A C-string containing the value of the specified environment variable.
+ */
+DALI_IMPORT_API const char * GetEnvironmentVariable( const char * variable );
+
+} // namespace EnvironmentVariable
+
+} // namespace Dali
+
+#endif /*__DALI_ ENVIRONMENT_VARIABLE_H__ */
