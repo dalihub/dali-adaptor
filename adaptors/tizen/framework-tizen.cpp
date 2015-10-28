18e20762d22e38c4e6ddffa9cfd53e1b14ce3c2c
taeyoon <taeyoon0.lee@samsung.com>
2015-10-28 14:46:51 +0900

    Revert "Revert "[3.0] Fix crash in wayland""
    This reverts commit 8109a5fc49750c6d66dc170aa3795b4b263f362a.
    
    Change-Id: Iaa3ef3e79ebcdab6df92885c73f95ec83552bf6c

--- adaptors/tizen/framework-tizen.cpp
+++ adaptors/tizen/framework-tizen.cpp
@@ -26,6 +26,7 @@
 #ifdef OVER_TIZEN_SDK_2_2
 #include <system_info.h>
 #include <app_control_internal.h>
+#include <bundle_internal.h>
 #endif
 
 #include <dali/integration-api/debug.h>
@@ -90,8 +91,6 @@ struct Framework::Impl
 #else
     mEventCallback.app_control = AppControl;
 
-    ui_app_add_event_handler(&handlers[APP_EVENT_LOW_BATTERY], APP_EVENT_LOW_BATTERY, AppBatteryLow, data);
-    ui_app_add_event_handler(&handlers[APP_EVENT_LOW_MEMORY], APP_EVENT_LOW_MEMORY, AppMemoryLow, data);
     ui_app_add_event_handler(&handlers[APP_EVENT_DEVICE_ORIENTATION_CHANGED], APP_EVENT_DEVICE_ORIENTATION_CHANGED, AppDeviceRotated, data);
     ui_app_add_event_handler(&handlers[APP_EVENT_LANGUAGE_CHANGED], APP_EVENT_LANGUAGE_CHANGED, AppLanguageChanged, data);
     ui_app_add_event_handler(&handlers[APP_EVENT_REGION_FORMAT_CHANGED], APP_EVENT_REGION_FORMAT_CHANGED, AppRegionChanged, data);
@@ -315,6 +314,7 @@ void Framework::Run()
   {
     DALI_LOG_ERROR("Framework::Run(), ui_app_main() is failed. err = %d", ret);
   }
+
 #endif
 
   mRunning = false;
@@ -322,7 +322,11 @@ void Framework::Run()
 
 void Framework::Quit()
 {
+#ifndef OVER_TIZEN_SDK_2_2
   app_efl_exit();
+#else
+  ui_app_exit();
+#endif
 }
 
 bool Framework::IsMainLoopRunning()
