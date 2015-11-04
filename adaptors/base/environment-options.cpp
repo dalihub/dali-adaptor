tizenorg/platform.core.uifw.dali-adaptor

Moved thread control functionality into a separate folder & added threading mode environment variable
Use DALI_THREADING_MODE to control which threading mode to use:
 - 0: MultiThreaded: Event, VSync, Update & Render ALL on separate threads.
 - 1: SingleThreaded: ALL functionality on the SAME thread.
 - 2: CombinedUpdateRender: Three threads: Event, VSync & Combined Update/Render

Only Mode 0 supported in this patch, modes 1 & 2 activated in later patches.

Change-Id: Ib45e7f5305ae56e9bd79def81a61503998e34824

adaptors/base/environment-options.cpp
@@ -103,7 +103,8 @@ EnvironmentOptions::EnvironmentOptions()
   mPanMinimumEvents(-1),
   mGlesCallTime(0),
   mWindowWidth( 0 ),
-  mWindowHeight( 0 )
+  mWindowHeight( 0 ),
+  mThreadingMode( ThreadingMode::MULTI_THREADED )
 {
   ParseEnvironmentOptions();
 }
@@ -234,6 +235,11 @@ const std::string& EnvironmentOptions::GetWindowClassName() const
   return mWindowClassName;
 }
 
+ThreadingMode::Type EnvironmentOptions::GetThreadingMode() const
+{
+  return mThreadingMode;
+}
+
 bool EnvironmentOptions::PerformanceServerRequired() const
 {
   return ( ( GetPerformanceStatsLoggingOptions() > 0) ||
@@ -345,6 +351,21 @@ void EnvironmentOptions::ParseEnvironmentOptions()
   if ( windowClassName )
   {
     mWindowClassName = windowClassName;
+  }
+
+  int threadingMode(0);
+  if ( GetIntegerEnvironmentVariable( DALI_THREADING_MODE, threadingMode ) )
+  {
+    switch( threadingMode )
+    {
+      case ThreadingMode::MULTI_THREADED:
+      case ThreadingMode::SINGLE_THREADED:
+      case ThreadingMode::COMBINED_UPDATE_RENDER:
+      {
+        mThreadingMode = static_cast< ThreadingMode::Type >( threadingMode );
+        break;
+      }
+    }
   }
 }
 

adaptors/base/thread-controller.cpp
@@ -19,12 +19,9 @@
 #include "thread-controller.h"
 
 // INTERNAL INCLUDES
-#include <base/update-thread.h>
-#include <base/render-thread.h>
-#include <base/thread-synchronization.h>
-#include <base/vsync-notifier.h>
-#include <base/interfaces/adaptor-internal-services.h>
 #include <base/environment-options.h>
+#include <base/thread-controller-interface.h>
+#include <base/multi-threaded/multi-thread-controller.h>
 
 namespace Dali
 {
@@ -36,103 +33,67 @@ namespace Adaptor
 {
 
 ThreadController::ThreadController( AdaptorInternalServices& adaptorInterfaces, const EnvironmentOptions& environmentOptions )
-: mAdaptorInterfaces( adaptorInterfaces ),
-  mUpdateThread( NULL ),
-  mRenderThread( NULL ),
-  mVSyncNotifier( NULL ),
-  mThreadSync( NULL ),
-  mNumberOfVSyncsPerRender( 1 )
+: mThreadControllerInterface( NULL )
 {
-  mThreadSync = new ThreadSynchronization( adaptorInterfaces, mNumberOfVSyncsPerRender );
-
-  mUpdateThread = new UpdateThread( *mThreadSync, adaptorInterfaces, environmentOptions );
-
-  mRenderThread = new RenderThread( *mThreadSync, adaptorInterfaces, environmentOptions );
-
-  mVSyncNotifier = new VSyncNotifier( *mThreadSync, adaptorInterfaces, environmentOptions );
-
-  // Set the thread-synchronization interface on the render-surface
-  RenderSurface* currentSurface = mAdaptorInterfaces.GetRenderSurfaceInterface();
-  if( currentSurface )
+  switch( environmentOptions.GetThreadingMode() )
   {
-    currentSurface->SetThreadSynchronization( *mThreadSync );
+    case ThreadingMode::MULTI_THREADED:
+    case ThreadingMode::SINGLE_THREADED:
+    case ThreadingMode::COMBINED_UPDATE_RENDER:
+    {
+      mThreadControllerInterface = new MultiThreadController( adaptorInterfaces, environmentOptions );
+    }
   }
 }
 
 ThreadController::~ThreadController()
 {
-  delete mVSyncNotifier;
-  delete mRenderThread;
-  delete mUpdateThread;
-  delete mThreadSync;
+  delete mThreadControllerInterface;
 }
 
 void ThreadController::Initialize()
 {
-  // Notify the synchronization object before starting the threads
-  mThreadSync->Initialise();
-
-  // We want to the threads to be set up before they start
-  mUpdateThread->Start();
-  mRenderThread->Start();
-  mVSyncNotifier->Start();
+  mThreadControllerInterface->Initialize();
 }
 
 void ThreadController::Start()
 {
-  mThreadSync->Start();
+  mThreadControllerInterface->Start();
 }
 
 void ThreadController::Pause()
 {
-  mThreadSync->Pause();
+  mThreadControllerInterface->Pause();
 }
 
 void ThreadController::Resume()
 {
-  mThreadSync->Resume();
+  mThreadControllerInterface->Resume();
 }
 
 void ThreadController::Stop()
 {
-  // Notify the synchronization object before stopping the threads
-  mThreadSync->Stop();
-
-  mVSyncNotifier->Stop();
-  mUpdateThread->Stop();
-  mRenderThread->Stop();
+  mThreadControllerInterface->Stop();
 }
 
 void ThreadController::RequestUpdate()
 {
-  mThreadSync->UpdateRequest();
+  mThreadControllerInterface->RequestUpdate();
 }
 
 void ThreadController::RequestUpdateOnce()
 {
-  // if we are paused, need to allow one update
-  mThreadSync->UpdateOnce();
+  mThreadControllerInterface->RequestUpdateOnce();
 }
 
 void ThreadController::ReplaceSurface( RenderSurface* newSurface )
 {
-  // Set the thread-syncronization on the new surface
-  newSurface->SetThreadSynchronization( *mThreadSync );
-
-  // tell render thread to start the replace. This call will block until the replace
-  // has completed.
-  RenderSurface* currentSurface = mAdaptorInterfaces.GetRenderSurfaceInterface();
-
-  // Ensure the current surface releases any locks to prevent deadlock.
-  currentSurface->StopRender();
-
-  mThreadSync->ReplaceSurface( newSurface );
+  mThreadControllerInterface->ReplaceSurface( newSurface );
 }
 
 void ThreadController::SetRenderRefreshRate(unsigned int numberOfVSyncsPerRender )
 {
-  mNumberOfVSyncsPerRender = numberOfVSyncsPerRender;
-  mThreadSync->SetRenderRefreshRate(numberOfVSyncsPerRender);
+  mThreadControllerInterface->SetRenderRefreshRate( numberOfVSyncsPerRender );
 }
 
 } // namespace Adaptor

dalihub/dali-adaptor

 - 0: SeparateUpdateRender: Event, VSync, Update & Render ALL on separate threads.

adaptors/base/environment-options.cpp
@@ -103,7 +103,8 @@ EnvironmentOptions::EnvironmentOptions()
   mPanMinimumEvents(-1),
   mGlesCallTime(0),
   mWindowWidth( 0 ),
-  mWindowHeight( 0 )
+  mWindowHeight( 0 ),
+  mThreadingMode( ThreadingMode::SEPARATE_UPDATE_RENDER )
 {
   ParseEnvironmentOptions();
 }
@@ -234,6 +235,11 @@ const std::string& EnvironmentOptions::GetWindowClassName() const
   return mWindowClassName;
 }
 
+ThreadingMode::Type EnvironmentOptions::GetThreadingMode() const
+{
+  return mThreadingMode;
+}
+
 bool EnvironmentOptions::PerformanceServerRequired() const
 {
   return ( ( GetPerformanceStatsLoggingOptions() > 0) ||
@@ -345,6 +351,21 @@ void EnvironmentOptions::ParseEnvironmentOptions()
   if ( windowClassName )
   {
     mWindowClassName = windowClassName;
+  }
+
+  int threadingMode(0);
+  if ( GetIntegerEnvironmentVariable( DALI_THREADING_MODE, threadingMode ) )
+  {
+    switch( threadingMode )
+    {
+      case ThreadingMode::SEPARATE_UPDATE_RENDER:
+      case ThreadingMode::COMBINED_UPDATE_RENDER:
+      case ThreadingMode::SINGLE_THREADED:
+      {
+        mThreadingMode = static_cast< ThreadingMode::Type >( threadingMode );
+        break;
+      }
+    }
   }
 }
 

adaptors/base/thread-controller.cpp
@@ -19,12 +19,9 @@
 #include "thread-controller.h"
 
 // INTERNAL INCLUDES
-#include <base/update-thread.h>
-#include <base/render-thread.h>
-#include <base/thread-synchronization.h>
-#include <base/vsync-notifier.h>
-#include <base/interfaces/adaptor-internal-services.h>
 #include <base/environment-options.h>
+#include <base/thread-controller-interface.h>
+#include <base/separate-update-render/separate-update-render-controller.h>
 
 namespace Dali
 {
@@ -36,103 +33,67 @@ namespace Adaptor
 {
 
 ThreadController::ThreadController( AdaptorInternalServices& adaptorInterfaces, const EnvironmentOptions& environmentOptions )
-: mAdaptorInterfaces( adaptorInterfaces ),
-  mUpdateThread( NULL ),
-  mRenderThread( NULL ),
-  mVSyncNotifier( NULL ),
-  mThreadSync( NULL ),
-  mNumberOfVSyncsPerRender( 1 )
+: mThreadControllerInterface( NULL )
 {
-  mThreadSync = new ThreadSynchronization( adaptorInterfaces, mNumberOfVSyncsPerRender );
-
-  mUpdateThread = new UpdateThread( *mThreadSync, adaptorInterfaces, environmentOptions );
-
-  mRenderThread = new RenderThread( *mThreadSync, adaptorInterfaces, environmentOptions );
-
-  mVSyncNotifier = new VSyncNotifier( *mThreadSync, adaptorInterfaces, environmentOptions );
-
-  // Set the thread-synchronization interface on the render-surface
-  RenderSurface* currentSurface = mAdaptorInterfaces.GetRenderSurfaceInterface();
-  if( currentSurface )
+  switch( environmentOptions.GetThreadingMode() )
   {
-    currentSurface->SetThreadSynchronization( *mThreadSync );
+    case ThreadingMode::SEPARATE_UPDATE_RENDER:
+    case ThreadingMode::COMBINED_UPDATE_RENDER:
+    case ThreadingMode::SINGLE_THREADED:
+    {
+      mThreadControllerInterface = new SeparateUpdateRenderController( adaptorInterfaces, environmentOptions );
+    }
   }
 }
 
 ThreadController::~ThreadController()
 {
-  delete mVSyncNotifier;
-  delete mRenderThread;
-  delete mUpdateThread;
-  delete mThreadSync;
+  delete mThreadControllerInterface;
 }
 
 void ThreadController::Initialize()
 {
-  // Notify the synchronization object before starting the threads
-  mThreadSync->Initialise();
-
-  // We want to the threads to be set up before they start
-  mUpdateThread->Start();
-  mRenderThread->Start();
-  mVSyncNotifier->Start();
+  mThreadControllerInterface->Initialize();
 }
 
 void ThreadController::Start()
 {
-  mThreadSync->Start();
+  mThreadControllerInterface->Start();
 }
 
 void ThreadController::Pause()
 {
-  mThreadSync->Pause();
+  mThreadControllerInterface->Pause();
 }
 
 void ThreadController::Resume()
 {
-  mThreadSync->Resume();
+  mThreadControllerInterface->Resume();
 }
 
 void ThreadController::Stop()
 {
-  // Notify the synchronization object before stopping the threads
-  mThreadSync->Stop();
-
-  mVSyncNotifier->Stop();
-  mUpdateThread->Stop();
-  mRenderThread->Stop();
+  mThreadControllerInterface->Stop();
 }
 
 void ThreadController::RequestUpdate()
 {
-  mThreadSync->UpdateRequest();
+  mThreadControllerInterface->RequestUpdate();
 }
 
 void ThreadController::RequestUpdateOnce()
 {
-  // if we are paused, need to allow one update
-  mThreadSync->UpdateOnce();
+  mThreadControllerInterface->RequestUpdateOnce();
 }
 
 void ThreadController::ReplaceSurface( RenderSurface* newSurface )
 {
-  // Set the thread-syncronization on the new surface
-  newSurface->SetThreadSynchronization( *mThreadSync );
-
-  // tell render thread to start the replace. This call will block until the replace
-  // has completed.
-  RenderSurface* currentSurface = mAdaptorInterfaces.GetRenderSurfaceInterface();
-
-  // Ensure the current surface releases any locks to prevent deadlock.
-  currentSurface->StopRender();
-
-  mThreadSync->ReplaceSurface( newSurface );
+  mThreadControllerInterface->ReplaceSurface( newSurface );
 }
 
 void ThreadController::SetRenderRefreshRate(unsigned int numberOfVSyncsPerRender )
 {
-  mNumberOfVSyncsPerRender = numberOfVSyncsPerRender;
-  mThreadSync->SetRenderRefreshRate(numberOfVSyncsPerRender);
+  mThreadControllerInterface->SetRenderRefreshRate( numberOfVSyncsPerRender );
 }
 
 } // namespace Adaptor