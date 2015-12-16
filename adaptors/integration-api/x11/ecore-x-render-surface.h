tizenorg/platform.core.uifw.dali-adaptor

PixmapSurface changes to double buffered pixmap.
Change-Id: I850a91ea662ff74c1c80be3a63c7965af21b53f4

adaptors/integration-api/x11/ecore-x-render-surface.h
@@ -70,13 +70,19 @@ protected:
   void Init( Any surface );
 
 public: // API
+  /**
+   * @brief Sets the render notification trigger to call when render thread prepares to draw a frame
+   *
+   * @param renderNotification to use
+   */
+  void SetPreRenderNotification(TriggerEventInterface* renderNotification);
 
   /**
    * @brief Sets the render notification trigger to call when render thread is completed a frame
    *
    * @param renderNotification to use
    */
-  void SetRenderNotification(TriggerEventInterface* renderNotification);
+  void SetPostRenderNotification(TriggerEventInterface* renderNotification);
 
   /**
    * @brief Get window handle
@@ -178,11 +184,12 @@ protected:
 
 protected: // Data
 
-  PositionSize                mPosition;           ///< Position
-  std::string                 mTitle;              ///< Title of window which shows from "xinfo -topvwins" command
-  TriggerEventInterface*      mRenderNotification; ///< Render notification trigger
-  ColorDepth                  mColorDepth;         ///< Color depth of surface (32 bit or 24 bit)
-  bool                        mOwnSurface;         ///< Whether we own the surface (responsible for deleting it)
+  PositionSize                mPosition;               ///< Position
+  std::string                 mTitle;                  ///< Title of window which shows from "xinfo -topvwins" command
+  TriggerEventInterface*      mPreRenderNotification;  ///< Render notification trigger
+  TriggerEventInterface*      mPostRenderNotification; ///< Render notification trigger
+  ColorDepth                  mColorDepth;             ///< Color depth of surface (32 bit or 24 bit)
+  bool                        mOwnSurface;             ///< Whether we own the surface (responsible for deleting it)
 };
 
 } // namespace ECore

dalihub/dali-adaptor

Change-Id: Ia8efa3ca09c9320ed2a1e9143a85e48418d56b77

adaptors/integration-api/x11/ecore-x-render-surface.h
@@ -178,11 +178,11 @@ protected:
 
 protected: // Data
 
-  PositionSize                mPosition;           ///< Position
-  std::string                 mTitle;              ///< Title of window which shows from "xinfo -topvwins" command
-  TriggerEventInterface*      mRenderNotification; ///< Render notification trigger
-  ColorDepth                  mColorDepth;         ///< Color depth of surface (32 bit or 24 bit)
-  bool                        mOwnSurface;         ///< Whether we own the surface (responsible for deleting it)
+  PositionSize                mPosition;               ///< Position
+  std::string                 mTitle;                  ///< Title of window which shows from "xinfo -topvwins" command
+  TriggerEventInterface*      mRenderNotification;     ///< Render notification trigger
+  ColorDepth                  mColorDepth;             ///< Color depth of surface (32 bit or 24 bit)
+  bool                        mOwnSurface;             ///< Whether we own the surface (responsible for deleting it)
 };
 
 } // namespace ECore