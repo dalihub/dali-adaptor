cda14c50fedd579b0fe07247b8efc0b1a91c6d7d
Tom Robinson <tom.robinson@samsung.com>
2016-01-11 16:49:57 +0000

    Added ASTC Native file format loader
    ASTC can be wrapped in a KTX file, but also has its own native file format.
    This patch provides support for the native ASTC file format (.astc).
    
    Textures can be generated using tools (Eg. Mali texture compression tool).
    
    Note: ASTC is "OpenGL friendly" in that the textures are pre-flipped vertically.
    In DALi there is currently not a differenciation between different texture compression formats in that the data (once the header is extracted) is given to the hardware as-is.
    For this reason textures must currently be pre-flipped vertically (before compression) when using the native .astc file format with DALI.
    
    An example .astc file is included as part of the unit tests.
    
    Change-Id: I574961bc184d68d3ec722be3c682ca2a2b7ed0ce

--- automated-tests/src/dali-adaptor-internal/utc-Dali-CompressedTextures.cpp
+++ automated-tests/src/dali-adaptor-internal/utc-Dali-CompressedTextures.cpp
@@ -21,11 +21,16 @@
 #include <vector>
 #include <dali-test-suite-utils.h>
 #include "platform-abstractions/tizen/image-loaders/loader-ktx.h"
+#include "platform-abstractions/tizen/image-loaders/loader-astc.h"
 
 // INTERNAL INCLUDES
 #include "image-loaders.h"
 
 using namespace Dali;
+
+// Pre-define loader functions for each image type being tested (as they are reused in different tests).
+static const LoadFunctions KtxLoaders(  TizenPlatform::LoadKtxHeader,  TizenPlatform::LoadBitmapFromKtx  );
+static const LoadFunctions AstcLoaders( TizenPlatform::LoadAstcHeader, TizenPlatform::LoadBitmapFromAstc );
 
 /**
  * This class encapsulates knowledge of testing compressed files.
@@ -36,8 +41,20 @@ class KtxTestFixture
 {
   public:
 
-    KtxTestFixture() {}
-    ~KtxTestFixture() {}
+    /**
+     * Constructor.
+     * Sets up the fixture.
+     */
+    KtxTestFixture( void )
+    {
+    }
+
+    /**
+     * Destructor.
+     */
+    ~KtxTestFixture()
+    {
+    }
 
     /**
      * This struct contains any per-test parameters.
@@ -45,12 +62,14 @@ class KtxTestFixture
      */
     struct TestEntry
     {
-        std::string filename; ///< Name of the compressed texture KTX file to load.
-        int expectedWidth;    ///< The width the texture should be.
-        int expectedHeight;   ///< The height the KTX texture should be.
+        LoadFunctions loadFunctions;    ///< Used to parse the header of a given type of image.
+        std::string filename;           ///< Name of the compressed texture KTX file to load.
+        int expectedWidth;              ///< The width the texture should be.
+        int expectedHeight;             ///< The height the KTX texture should be.
 
-        TestEntry( std::string newFilename, int newExpectedWidth, int newExpectedHeight )
-        : filename( newFilename ),
+        TestEntry( const LoadFunctions& newLoadFunctions, std::string newFilename, int newExpectedWidth, int newExpectedHeight )
+        : loadFunctions( newLoadFunctions ),
+          filename( newFilename ),
           expectedWidth( newExpectedWidth ),
           expectedHeight( newExpectedHeight )
         {
@@ -102,7 +121,8 @@ class KtxTestFixture
       unsigned int width( 0 ), height( 0 );
       const Dali::TizenPlatform::ImageLoader::Input input( fileDescriptor );
 
-      DALI_TEST_CHECK( TizenPlatform::LoadKtxHeader( input, width, height ) );
+      // Use the given loader to parse the image header.
+      DALI_TEST_CHECK( testEntry.loadFunctions.header( input, width, height ) );
 
       DALI_TEST_EQUALS( width,  testEntry.expectedWidth,  TEST_LOCATION );
       DALI_TEST_EQUALS( height, testEntry.expectedHeight, TEST_LOCATION );
@@ -110,17 +130,18 @@ class KtxTestFixture
 
   private:
 
-    TestContainer mTests;    ///< Holds all tests to be run.
+    TestContainer mTests;         ///< Holds all tests to be run.
 
 };
 
+// KTX files (KTX is a wrapper, so can contain different compressed texture types):
 
 int UtcDaliKtxLoaderETC(void)
 {
   KtxTestFixture fixture;
 
-  fixture.AddTest( KtxTestFixture::TestEntry( TEST_IMAGE_DIR "/fractal-compressed-ETC1_RGB8_OES-45x80.ktx", 45u, 80u ) );
-  fixture.AddTest( KtxTestFixture::TestEntry( TEST_IMAGE_DIR "/fractal-compressed-RGB8_ETC2-45x80.ktx", 45u, 80u ) );
+  fixture.AddTest( KtxTestFixture::TestEntry( KtxLoaders, TEST_IMAGE_DIR "/fractal-compressed-ETC1_RGB8_OES-45x80.ktx", 45u, 80u ) );
+  fixture.AddTest( KtxTestFixture::TestEntry( KtxLoaders, TEST_IMAGE_DIR "/fractal-compressed-RGB8_ETC2-45x80.ktx", 45u, 80u ) );
 
   fixture.RunTests();
 
@@ -131,7 +152,7 @@ int UtcDaliKtxLoaderPVRTC(void)
 {
   KtxTestFixture fixture;
 
-  fixture.AddTest( KtxTestFixture::TestEntry( TEST_IMAGE_DIR "/fractal-compressed-RGB_PVRTC_4BPPV1_IMG-32x64.ktx", 32u, 64u ) );
+  fixture.AddTest( KtxTestFixture::TestEntry( KtxLoaders, TEST_IMAGE_DIR "/fractal-compressed-RGB_PVRTC_4BPPV1_IMG-32x64.ktx", 32u, 64u ) );
 
   fixture.RunTests();
 
@@ -142,7 +163,7 @@ int UtcDaliKtxLoaderEAC(void)
 {
   KtxTestFixture fixture;
 
-  fixture.AddTest( KtxTestFixture::TestEntry( TEST_IMAGE_DIR "/fractal-compressed-R11_EAC-45x80.ktx", 45u, 80u ) );
+  fixture.AddTest( KtxTestFixture::TestEntry( KtxLoaders, TEST_IMAGE_DIR "/fractal-compressed-R11_EAC-45x80.ktx", 45u, 80u ) );
 
   fixture.RunTests();
 
@@ -153,10 +174,24 @@ int UtcDaliKtxLoaderASTC(void)
 {
   KtxTestFixture fixture;
 
-  fixture.AddTest( KtxTestFixture::TestEntry( TEST_IMAGE_DIR "/fractal-compressed-RGBA_ASTC_4x4_KHR-32x64.ktx", 32u, 64u ) );
-  fixture.AddTest( KtxTestFixture::TestEntry( TEST_IMAGE_DIR "/fractal-compressed-SRBG8_ALPHA8_ASTC_4x4_KHR-32x64.ktx", 32u, 64u ) );
+  fixture.AddTest( KtxTestFixture::TestEntry( KtxLoaders, TEST_IMAGE_DIR "/fractal-compressed-RGBA_ASTC_4x4_KHR-32x64.ktx", 32u, 64u ) );
+  fixture.AddTest( KtxTestFixture::TestEntry( KtxLoaders, TEST_IMAGE_DIR "/fractal-compressed-SRBG8_ALPHA8_ASTC_4x4_KHR-32x64.ktx", 32u, 64u ) );
 
   fixture.RunTests();
 
   END_TEST;
 }
+
+
+// ASTC (Native) files:
+int UtcDaliAstcLoader(void)
+{
+  KtxTestFixture fixture;
+
+  fixture.AddTest( KtxTestFixture::TestEntry( AstcLoaders, TEST_IMAGE_DIR "/fractal-compressed-RGBA_ASTC_4x4_KHR-32x64.astc", 32u, 64u ) );
+
+  fixture.RunTests();
+
+  END_TEST;
+}
+
