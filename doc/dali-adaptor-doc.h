c1fdc0701a0eb9cfb0a6e64ca80ae5927f017df7
Yoonsang Lee <ysang114.lee@samsung.com>
2016-01-28 08:20:38 +0900

    Update doxygen groups and overview description
    - Update overview of DALi adaptor
    
    Change-Id: Iede36ea40f2a8a85acf0fbe00ab886aaecdc0af0

--- doc/dali-adaptor-doc.h
+++ doc/dali-adaptor-doc.h
@@ -1,13 +1,47 @@
 #ifndef __DALI_ADAPTOR_DOC_H__
 #define __DALI_ADAPTOR_DOC_H__
 
+/*
+ * Copyright (c) 2016 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 /**
  * @defgroup dali_adaptor DALi Adaptor
  *
- * @brief This module is a platform adaptation layer. It initializes and sets up DALi appropriately.
- * The module provides many platform-related services with its internal module,
+ * @brief DALi Adaptor is a platform adaptation layer.
+ *
+ * It initializes and sets up DALi appropriately and
+ * provides many platform-related services with its internal module,
  * platform abstraction. Several signals can be connected to it to keep you informed when
  * certain platform-related activities occur.
+ *
+ * @section dali_adaptor_overview Overview
+ *
+ * DALi Adaptor consists of the following groups of API:
+ *
+ * <table>
+ * <tr>
+ *    <th>API Group</th>
+ *    <th>Description</th>
+ * </tr>
+ * <tr>
+ *    <td>@ref dali_adaptor_framework</td>
+ *    <td>Classes for the adaption layer.</td>
+ * </tr>
+ * </table>
  *
  * @ingroup dali
  * @{
