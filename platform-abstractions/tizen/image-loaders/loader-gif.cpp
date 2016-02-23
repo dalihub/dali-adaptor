ff274a8d0ece5bfd1e2891d6c8550f398eadd07c
dongsug.song <dongsug.song@samsung.com>
2016-02-23 22:10:43 +0900

    Revert "[3.0] giflib upgrade"
    This reverts commit 08bbc232382cacf1c106543fe5db51996bbc665e.
    
    Change-Id: I1a715832b2476c540c7f7a59c965574259323a91

--- platform-abstractions/tizen/image-loaders/loader-gif.cpp
+++ platform-abstractions/tizen/image-loaders/loader-gif.cpp
@@ -47,15 +47,10 @@ struct AutoCleanupGif
     if(NULL != gifInfo)
     {
       // clean up GIF resources
-      int errorCode = 0; //D_GIF_SUCCEEDED is 0
-      DGifCloseFile(gifInfo, &errorCode);
-
-      if( errorCode )
-      {
-        DALI_LOG_ERROR( "GIF Loader: DGifCloseFile Error. Code: %d\n", errorCode);
-      }
+      DGifCloseFile(gifInfo);
     }
   }
+
   GifFileType*& gifInfo;
 };
 
@@ -103,12 +98,10 @@ int ReadDataFromGif(GifFileType *gifInfo, GifByteType *data, int length)
 /// Loads the GIF Header.
 bool LoadGifHeader(FILE *fp, unsigned int &width, unsigned int &height, GifFileType** gifInfo)
 {
-  int errorCode = 0; //D_GIF_SUCCEEDED is 0
-  *gifInfo = DGifOpen(reinterpret_cast<void*>(fp), ReadDataFromGif, &errorCode);
+  *gifInfo = DGifOpen(reinterpret_cast<void*>(fp), ReadDataFromGif);
 
-  if ( !(*gifInfo) || errorCode )
+  if ( !(*gifInfo) )
   {
-    DALI_LOG_ERROR( "GIF Loader: DGifOpen Error. Code: %d\n", errorCode);
     return false;
   }
 
@@ -249,7 +242,7 @@ bool HandleExtensionRecordType( GifFileType* gifInfo )
   GifByteType *extensionByte( NULL );
 
   // Not really interested in the extensions so just skip them unless there is an error.
-  for ( int extRetCode = DGifGetExtension( gifInfo, &image.ExtensionBlocks->Function, &extensionByte );
+  for ( int extRetCode = DGifGetExtension( gifInfo, &image.Function, &extensionByte );
         extensionByte != NULL;
         extRetCode = DGifGetExtensionNext( gifInfo, &extensionByte ) )
   {
