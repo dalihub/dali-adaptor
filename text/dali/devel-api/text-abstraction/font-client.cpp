tizenorg/platform.core.uifw.dali-adaptor

Vector-based text rendering
Change-Id: I1919a3fc7bddf399927d4ffc082af716b9f1032f

text/dali/devel-api/text-abstraction/font-client.cpp
@@ -148,14 +148,19 @@ GlyphIndex FontClient::GetGlyphIndex( FontId fontId, Character charcode )
   return GetImplementation(*this).GetGlyphIndex( fontId, charcode );
 }
 
-bool FontClient::GetGlyphMetrics( GlyphInfo* array, uint32_t size, bool horizontal, int maxFixedSize )
+bool FontClient::GetGlyphMetrics( GlyphInfo* array, uint32_t size, GlyphType type, bool horizontal, int maxFixedSize )
 {
-  return GetImplementation(*this).GetGlyphMetrics( array, size, horizontal, maxFixedSize );
+  return GetImplementation(*this).GetGlyphMetrics( array, size, type, horizontal, maxFixedSize );
 }
 
 BufferImage FontClient::CreateBitmap( FontId fontId, GlyphIndex glyphIndex )
 {
   return GetImplementation(*this).CreateBitmap( fontId, glyphIndex );
+}
+
+void FontClient::CreateGlyphyBlob( FontId fontId, GlyphIndex glyphIndex, GlyphyBlob*& blob, unsigned int& blobLength, unsigned int& nominalWidth, unsigned int& nominalHeight )
+{
+  GetImplementation(*this).CreateGlyphyBlob( fontId, glyphIndex, blob, blobLength, nominalWidth, nominalHeight );
 }
 
 const GlyphInfo& FontClient::GetEllipsisGlyph( PointSize26Dot6 pointSize )

dalihub/dali-adaptor

text/dali/devel-api/text-abstraction/font-client.cpp
@@ -148,14 +148,19 @@ GlyphIndex FontClient::GetGlyphIndex( FontId fontId, Character charcode )
   return GetImplementation(*this).GetGlyphIndex( fontId, charcode );
 }
 
-bool FontClient::GetGlyphMetrics( GlyphInfo* array, uint32_t size, bool horizontal, int desiredFixedSize )
+bool FontClient::GetGlyphMetrics( GlyphInfo* array, uint32_t size, GlyphType type, bool horizontal, int desiredFixedSize )
 {
-  return GetImplementation(*this).GetGlyphMetrics( array, size, horizontal, desiredFixedSize );
+  return GetImplementation(*this).GetGlyphMetrics( array, size, type, horizontal, desiredFixedSize );
 }
 
 BufferImage FontClient::CreateBitmap( FontId fontId, GlyphIndex glyphIndex )
 {
   return GetImplementation(*this).CreateBitmap( fontId, glyphIndex );
+}
+
+void FontClient::CreateVectorBlob( FontId fontId, GlyphIndex glyphIndex, VectorBlob*& blob, unsigned int& blobLength, unsigned int& nominalWidth, unsigned int& nominalHeight )
+{
+  GetImplementation(*this).CreateVectorBlob( fontId, glyphIndex, blob, blobLength, nominalWidth, nominalHeight );
 }
 
 const GlyphInfo& FontClient::GetEllipsisGlyph( PointSize26Dot6 pointSize )