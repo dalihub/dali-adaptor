tizenorg/platform.core.uifw.dali-adaptor

Changes following dali-core patch "Wrapped ShaderHints enum inside a struct"
Change-Id: I608bfff71c04f84db6f243bfaaccf9c91804b721

adaptors/ecore/common/ecore-indicator-impl.cpp
@@ -513,7 +513,7 @@ void Indicator::SetOpacityMode( Dali::Window::IndicatorBgOpacity mode )
     {
       if( !mBackgroundShader )
       {
-        mBackgroundShader = Dali::Shader::New( BACKGROUND_VERTEX_SHADER, BACKGROUND_FRAGMENT_SHADER, Dali::Shader::HINT_OUTPUT_IS_TRANSPARENT );
+        mBackgroundShader = Dali::Shader::New( BACKGROUND_VERTEX_SHADER, BACKGROUND_FRAGMENT_SHADER, Dali::Shader::Hints::OUTPUT_IS_TRANSPARENT );
       }
 
       mBackgroundRenderer = Dali::Renderer::New( geometry, mBackgroundShader );

adaptors/emscripten/wrappers/dali-wrapper.cpp
@@ -855,10 +855,10 @@ EMSCRIPTEN_BINDINGS(dali_wrapper)
     .value("HINT_DOESNT_MODIFY_GEOMETRY", Dali::ShaderEffect::HINT_DOESNT_MODIFY_GEOMETRY)
 ;
 
-  enum_<Dali::Shader::ShaderHints>("ShaderHints")
-    .value("HINT_NONE",                      Dali::Shader::HINT_NONE)
-    .value("HINT_OUTPUT_IS_TRANSPARENT",     Dali::Shader::HINT_OUTPUT_IS_TRANSPARENT)
-    .value("HINT_MODIFIES_GEOMETRY",         Dali::Shader::HINT_MODIFIES_GEOMETRY)
+  enum_<Dali::Shader::Hints::Value>("ShaderHints")
+    .value("NONE",                      Dali::Shader::Hints::NONE)
+    .value("OUTPUT_IS_TRANSPARENT",     Dali::Shader::Hints::OUTPUT_IS_TRANSPARENT)
+    .value("MODIFIES_GEOMETRY",         Dali::Shader::Hints::MODIFIES_GEOMETRY)
 ;
 
   enum_<Dali::Animation::EndAction>("EndAction")

dalihub/dali-adaptor

adaptors/ecore/common/ecore-indicator-impl.cpp
@@ -513,7 +513,7 @@ void Indicator::SetOpacityMode( Dali::Window::IndicatorBgOpacity mode )
     {
       if( !mBackgroundShader )
       {
-        mBackgroundShader = Dali::Shader::New( BACKGROUND_VERTEX_SHADER, BACKGROUND_FRAGMENT_SHADER, Dali::Shader::HINT_OUTPUT_IS_TRANSPARENT );
+        mBackgroundShader = Dali::Shader::New( BACKGROUND_VERTEX_SHADER, BACKGROUND_FRAGMENT_SHADER, Dali::Shader::Hint::OUTPUT_IS_TRANSPARENT );
       }
 
       mBackgroundRenderer = Dali::Renderer::New( geometry, mBackgroundShader );

adaptors/emscripten/wrappers/dali-wrapper.cpp
@@ -855,10 +855,10 @@ EMSCRIPTEN_BINDINGS(dali_wrapper)
     .value("HINT_DOESNT_MODIFY_GEOMETRY", Dali::ShaderEffect::HINT_DOESNT_MODIFY_GEOMETRY)
 ;
 
-  enum_<Dali::Shader::ShaderHints>("ShaderHints")
-    .value("HINT_NONE",                      Dali::Shader::HINT_NONE)
-    .value("HINT_OUTPUT_IS_TRANSPARENT",     Dali::Shader::HINT_OUTPUT_IS_TRANSPARENT)
-    .value("HINT_MODIFIES_GEOMETRY",         Dali::Shader::HINT_MODIFIES_GEOMETRY)
+  enum_<Dali::Shader::Hint::Value>("ShaderHints")
+    .value("NONE",                      Dali::Shader::Hint::NONE)
+    .value("OUTPUT_IS_TRANSPARENT",     Dali::Shader::Hint::OUTPUT_IS_TRANSPARENT)
+    .value("MODIFIES_GEOMETRY",         Dali::Shader::Hint::MODIFIES_GEOMETRY)
 ;
 
   enum_<Dali::Animation::EndAction>("EndAction")