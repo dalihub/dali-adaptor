3f72898c6fdc131a7167e57b6ba3740ff18bba8e
Seoyeon Kim <seoyeon2.kim@samsung.com>
2016-08-03 16:14:44 +0900

    Add descriptions and example codes
    Change-Id: If6318c0234d7ab7dd590b01dbbe7a4668b817ae8
    Signed-off-by: Seoyeon Kim <seoyeon2.kim@samsung.com>

--- adaptors/public-api/adaptor-framework/application.h
+++ adaptors/public-api/adaptor-framework/application.h
@@ -145,6 +145,7 @@ public:
    * @param[in,out]  argc        A pointer to the number of arguments
    * @param[in,out]  argv        A pointer the the argument list
    * @param[in]      stylesheet  The path to user defined theme file
+   * @note If the stylesheet is not specified, then the library's default stylesheet will not be overridden.
    */
   static Application New( int* argc, char **argv[], const std::string& stylesheet );
 
@@ -156,6 +157,7 @@ public:
    * @param[in,out]  argv        A pointer the the argument list
    * @param[in]      stylesheet  The path to user defined theme file
    * @param[in]      windowMode  A member of WINDOW_MODE
+   * @note If the stylesheet is not specified, then the library's default stylesheet will not be overridden.
    */
   static Application New( int* argc, char **argv[], const std::string& stylesheet, WINDOW_MODE windowMode );
 
@@ -277,6 +279,8 @@ public: // Stereoscopy
   /**
    * @brief Set the stereo base (eye separation) for Stereoscopic 3D
    *
+   * The stereo base is the distance in millimetres between the eyes. Typical values are
+   * between 50mm and 70mm. The default value is 65mm.
    * @SINCE_1_0.0
    * @param[in] stereoBase The stereo base (eye separation) for Stereoscopic 3D
    */
