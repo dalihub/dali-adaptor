487d11c71da6abf72736cbfbd96ddf025c28f982
taeyoon <taeyoon0.lee@samsung.com>
2016-08-09 17:25:11 +0900

    Revert "Use remarks tag for alarm.set privilege info"
    This reverts commit 088d4b5b11b6c8c80e41b423a999ed3a8dade339.
    
    Change-Id: I0977d363bc3d1578fa0511a8efe91130bb1edf55

--- adaptors/wearable/watch/watch-application.h
+++ adaptors/wearable/watch/watch-application.h
@@ -172,8 +172,6 @@ public:
    * @endcode
    * time(watch time handle) will not be available after returning this callback. It will be freed by the framework.
    * @SINCE_1_1.37
-   * @remarks http://tizen.org/privilege/alarm.set privilege is needed to receive ambient ticks at each minute.
-   * The AmbientTickSignal() will be ignored if your app doesn't have the privilege
    */
   WatchTimeSignal& AmbientTickSignal();
 
@@ -185,6 +183,8 @@ public:
    * @endcode
    * ambient_mode If true the device enters the ambient mode, otherwise false
    * @SINCE_1_1.37
+   * @PRIVLEVEL_PUBLIC
+   * @PRIVILEGE_ALARM_SET
    */
   WatchBoolSignal& AmbientChangedSignal();
 
