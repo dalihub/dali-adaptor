674c4f534915963930ebd44f290e7f367773b1db
Adeel Kazmi <adeel.kazmi@samsung.com>
2017-01-12 15:46:56 +0000

    Remove some meaningless log output & convert other log warnings to log info
    Change-Id: Ifeb876e6f9ac9e25d455db58e8464ddf8ac5d95b

--- adaptors/x11/imf-manager-impl-x.cpp
+++ adaptors/x11/imf-manager-impl-x.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014 Samsung Electronics Co., Ltd.
+ * Copyright (c) 2017 Samsung Electronics Co., Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -215,12 +215,12 @@ void ImfManager::CreateContext( Ecore_X_Window ecoreXwin )
     }
     else
     {
-      DALI_LOG_WARNING("IMF Unable to get IMF Context\n");
+      DALI_LOG_INFO( gLogFilter, Debug::General, "IMF Unable to get IMF Context\n");
     }
   }
   else
   {
-    DALI_LOG_WARNING("IMF Unable to get IMF Context\n");
+    DALI_LOG_INFO( gLogFilter, Debug::General, "IMF Unable to get IMF Context\n");
   }
 }
 
