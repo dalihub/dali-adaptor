adc1cf240fcdb5acf24850411b85b76b39dd992f
Francisco Santos <f1.santos@samsung.com>
2017-01-13 17:21:45 +0000

    Functions that return value types should not return them as const.
    It is ignored by the compiler and produces warnings.
    
    Change-Id: I8f46f597718f72c15a4ca17f98751adf00057c20

--- adaptors/base/performance-logging/statistics/stat-context-manager.cpp
+++ adaptors/base/performance-logging/statistics/stat-context-manager.cpp
@@ -174,7 +174,7 @@ void StatContextManager::SetLoggingFrequency( unsigned int logFrequency,
     context->SetLogFrequency( logFrequency );
   }
 }
-const char* const StatContextManager::GetContextName(PerformanceInterface::ContextId contextId) const
+const char* StatContextManager::GetContextName(PerformanceInterface::ContextId contextId) const
 {
   StatContext* context = GetContext(contextId);
   if( context )
@@ -184,7 +184,7 @@ const char* const StatContextManager::GetContextName(PerformanceInterface::Conte
   return "context not found";
 }
 
-const char* const StatContextManager::GetMarkerDescription( PerformanceInterface::MarkerType type, PerformanceInterface::ContextId contextId ) const
+const char* StatContextManager::GetMarkerDescription( PerformanceInterface::MarkerType type, PerformanceInterface::ContextId contextId ) const
 {
   StatContext* context = GetContext(contextId);
   if( context )
