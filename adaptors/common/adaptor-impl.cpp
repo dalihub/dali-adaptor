06cf40aa46d2c142deb66692347251f1526b7f3e
Jinho, Lee <jeano.lee@samsung.com>
2017-09-28 13:35:40 +0900

    Revert "[4.0] Call RequestUpdateOnce after pause callback"
    This reverts commit e33f884b6b613033d8a4fd270a33c9d444933061.
    
    Change-Id: Ie7832feadd2dcfe3b753f17222ef336bf434fdab

--- adaptors/common/adaptor-impl.cpp
+++ adaptors/common/adaptor-impl.cpp
@@ -304,9 +304,6 @@ void Adaptor::Pause()
     mThreadController->Pause();
     mCore->Suspend();
     mState = PAUSED;
-
-    // Process remained events and rendering in the update thread
-    RequestUpdateOnce();
   }
 }
 
