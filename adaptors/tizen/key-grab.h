922f063d9fdb5b2fd295dc2ff5386e22592bf0a7
Seoyeon Kim <seoyeon2.kim@samsung.com>
2017-11-28 14:10:38 +0900

    Update doxygen comment
    - Explicit link request to 'Vector' could not be resolved.
      So, changed the link to 'Dali::Vector'.
    
    Change-Id: Ic1892d455b01e0810b9b60bd81c9df1e92882bb5
    Signed-off-by: Seoyeon Kim <seoyeon2.kim@samsung.com>

--- adaptors/tizen/key-grab.h
+++ adaptors/tizen/key-grab.h
@@ -113,7 +113,7 @@ DALI_IMPORT_API bool UngrabKey( Window window, Dali::KEY daliKey );
 
 /**
  * @PLATFORM
- * @brief Grabs the list of keys specified by @Dali::Vector of keys for @a window in @Vector of grabModes.
+ * @brief Grabs the list of keys specified by @Dali::Vector of keys for @a window in @Dali::Vector of grabModes.
  *
  * @details This function can be used for following example scenarios:
  * - TV - A user might want to change the volume or channel of the background TV contents while focusing on the foregrund app.
