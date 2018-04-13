75ab892f2ffcc123a2f1fa31854d02946c2d3a11
Seoyeon Kim <seoyeon2.kim@samsung.com>
2018-04-13 11:08:15 +0900

    Move TtsPlayer internal files separately
    - TtsPlayer internal files are in tizen-common folder,
      whereas AccessibilityAdaptor files are separated on profiles.
    - So, all tizen profiles except common profile could not get TtsPlayer object.
    - Make 'tizen' folder and move TtsPlayer internal files independently.
    
    Change-Id: Ie70232999203114006f72dcbd227c291ee93dd69
    Signed-off-by: Seoyeon Kim <seoyeon2.kim@samsung.com>

--- dali/internal/accessibility/tizen/tts-player-factory-tizen.cpp
+++ dali/internal/accessibility/tizen/tts-player-factory-tizen.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017 Samsung Electronics Co., Ltd.
+ * Copyright (c) 2018 Samsung Electronics Co., Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,7 +16,7 @@
  */
 
 #include <dali/internal/accessibility/common/tts-player-factory.h>
-#include <dali/internal/accessibility/tizen-common/tts-player-impl-tizen.h>
+#include <dali/internal/accessibility/tizen/tts-player-impl-tizen.h>
 
 namespace Dali
 {
