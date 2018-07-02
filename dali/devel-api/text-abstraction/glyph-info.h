fdd1a6fece5e32770f22769d2b6c5083be38b7ff
Sunghyun kim <scholb.kim@samsung.com>
2018-07-02 14:50:08 +0900

    Revert "[Tizen] Add codes for Dali Windows Backend"
    This reverts commit 476644dcbd177cbfa3da5576859ffb2535235cde.
    
    Change-Id: Ie83d23c1579d25729238468ebd86e3ac76506af4

--- dali/devel-api/text-abstraction/glyph-info.h
+++ dali/devel-api/text-abstraction/glyph-info.h
@@ -18,7 +18,7 @@
  *
  */
 
-// INTERNAL INCLUDES
+// EXTERNAL INCLUDES
 #include <dali/public-api/dali-adaptor-common.h>
 #include <dali/devel-api/text-abstraction/text-abstraction-definitions.h>
 
