424a9807ccca4b282bb0c6d2330e65165d929cdc
David Steele <david.steele@samsung.com>
2018-07-10 12:20:06 +0100

    Fixed constness for VkSurfaceFactory::Create arguments
    Change-Id: I7d13a6a7630bbe2004c00cb27b10d9b886889586

--- dali/internal/graphics/vulkan/wayland/vk-surface-wayland.cpp
+++ dali/internal/graphics/vulkan/wayland/vk-surface-wayland.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017 Samsung Electronics Co., Ltd.
+ * Copyright (c) 2018 Samsung Electronics Co., Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -58,8 +58,10 @@ VkSurfaceWayland::VkSurfaceWayland(::wl_display* display, ::wl_surface* surface)
 }
 
 
-vk::SurfaceKHR VkSurfaceWayland::Create(vk::Instance instance, vk::AllocationCallbacks* allocCallbacks,
-                                    vk::PhysicalDevice physicalDevice) const
+vk::SurfaceKHR VkSurfaceWayland::Create(
+  vk::Instance instance,
+  const vk::AllocationCallbacks* allocCallbacks,
+  vk::PhysicalDevice physicalDevice) const
 {
   vk::WaylandSurfaceCreateInfoKHR info;
 
