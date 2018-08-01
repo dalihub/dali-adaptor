aead83b18b539ee9e2ac9b4ec5faf85d6e2aae75
Victor Cebollada <v.cebollada@samsung.com>
2018-08-01 17:00:24 +0100

    FreeType fonts/library - Stop creating new instances.
    * The shaping tool creates a new instance
    of the FreeType library and the font faces instead of using the
    ones cached in the font client.
      It causes a bug in the WatchFace emulator when a new model is
    loaded (The text show the wrong glyphs shaped with the wrong font).
    
    Change-Id: I1c9ff437b9eed42ece2290642c279bc228ffa442
    Signed-off-by: Victor Cebollada <v.cebollada@samsung.com>

--- dali/internal/text/text-abstraction/font-client-plugin-impl.h
+++ dali/internal/text/text-abstraction/font-client-plugin-impl.h
@@ -323,6 +323,11 @@ struct FontClient::Plugin
    */
   bool IsColorGlyph( FontId fontId, GlyphIndex glyphIndex );
 
+  /**
+   * @copydoc Dali::TextAbstraction::Internal::FontClient::GetFreetypeFace()
+   */
+  FT_FaceRec_* GetFreetypeFace( FontId fontId );
+
 private:
 
   /**
