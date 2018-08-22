6ee343598a266ccc1ecf6d9e4c5c92305c36ebee
David Steele <david.steele@samsung.com>
2018-08-22 17:41:08 +0100

    Triggered Graphics lifecycle event handlers
    Change-Id: Ifdf9d45a6b0dcbd07d0f58c7542fa26a507edaf2

--- dali/internal/adaptor/common/combined-update-render-controller.cpp
+++ dali/internal/adaptor/common/combined-update-render-controller.cpp
@@ -625,7 +625,17 @@ bool CombinedUpdateRenderController::UpdateRenderReady( bool& useElapsedTime, bo
     // of the first frame.
     timeToSleepUntil = 0;
 
+    if( ! mUpdateRenderRunCount )
+    {
+      mGraphics.Pause();
+    }
+
     mUpdateRenderThreadWaitCondition.Wait( updateLock );
+
+    if( mUpdateRenderRunCount )
+    {
+      mGraphics.Resume();
+    }
 
     if( ! mUseElapsedTimeAfterWait )
     {
