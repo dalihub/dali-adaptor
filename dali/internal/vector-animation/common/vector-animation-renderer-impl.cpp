6b0eb61500ee97af7a2b0b9403e0bbf2786a4b4c
Heeyong Song <heeyong.song@samsung.com>
2018-10-17 14:30:48 +0900

    Add Vector animation renderer
    Change-Id: I6390df6ea8dd703ac479198c57d5b166ac15ab54

---
+++ dali/internal/vector-animation/common/vector-animation-renderer-impl.cpp
@@ -0,0 +1,92 @@
+/*
+ * Copyright (c) 2018 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+// CLASS HEADER
+#include <dali/internal/vector-animation/common/vector-animation-renderer-impl.h>
+
+// EXTERNAL INCLUDES
+#include <dali/public-api/object/type-registry.h>
+
+// INTERNAL INCLUDES
+
+namespace Dali
+{
+
+namespace Internal
+{
+
+namespace Adaptor
+{
+
+namespace // unnamed namespace
+{
+
+// Type Registration
+Dali::BaseHandle Create()
+{
+  return Dali::BaseHandle();
+}
+
+Dali::TypeRegistration type( typeid( Dali::VectorAnimationRenderer ), typeid( Dali::BaseHandle ), Create );
+
+} // unnamed namespace
+
+VectorAnimationRendererPtr VectorAnimationRenderer::New()
+{
+  VectorAnimationRendererPtr renderer = new VectorAnimationRenderer();
+  return renderer;
+}
+
+VectorAnimationRenderer::VectorAnimationRenderer()
+: mPlugin( std::string() )
+{
+}
+
+VectorAnimationRenderer::~VectorAnimationRenderer()
+{
+}
+
+void VectorAnimationRenderer::Initialize( const std::string& url, Dali::Renderer renderer, uint32_t width, uint32_t height )
+{
+  mPlugin.CreateRenderer( url, renderer, width, height );
+}
+
+bool VectorAnimationRenderer::StartRender()
+{
+  return mPlugin.StartRender();
+}
+
+void VectorAnimationRenderer::StopRender()
+{
+  mPlugin.StopRender();
+}
+
+void VectorAnimationRenderer::Render( uint32_t frameNumber )
+{
+  mPlugin.Render( frameNumber );
+}
+
+uint32_t VectorAnimationRenderer::GetTotalFrameNumber()
+{
+  return mPlugin.GetTotalFrameNumber();
+}
+
+} // namespace Adaptor
+
+} // namespace internal
+
+} // namespace Dali
---
+++ dali/internal/vector-animation/common/vector-animation-renderer-impl.h
@@ -0,0 +1,125 @@
+#ifndef DALI_INTERNAL_VECTOR_ANIMATION_RENDERER_IMPL_H
+#define DALI_INTERNAL_VECTOR_ANIMATION_RENDERER_IMPL_H
+
+/*
+ * Copyright (c) 2018 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+// EXTERNAL INCLUDES
+#include <dali/public-api/object/base-object.h>
+
+// INTERNAL INCLUDES
+#include <dali/devel-api/adaptor-framework/vector-animation-renderer.h>
+#include <dali/internal/vector-animation/common/vector-animation-renderer-plugin-proxy.h>
+
+namespace Dali
+{
+
+namespace Internal
+{
+
+namespace Adaptor
+{
+
+class VectorAnimationRenderer;
+using VectorAnimationRendererPtr = IntrusivePtr< VectorAnimationRenderer >;
+
+/**
+ * Dali internal VectorAnimationRenderer.
+ */
+class VectorAnimationRenderer : public BaseObject
+{
+public:
+
+  /**
+   * @brief Creates a VectorAnimationRenderer object.
+   *
+   * @param[in] url The url of the vector animation file
+   */
+  static VectorAnimationRendererPtr New();
+
+  /**
+   * @brief Initializes member data.
+   */
+  void Initialize( const std::string& url, Dali::Renderer renderer, uint32_t width, uint32_t height );
+
+  /**
+   * @copydoc Dali::VectorAnimationRenderer::StartRender()
+   */
+  bool StartRender();
+
+  /**
+   * @copydoc Dali::VectorAnimationRenderer::StopRender()
+   */
+  void StopRender();
+
+  /**
+   * @copydoc Dali::VectorAnimationRenderer::Render()
+   */
+  void Render( uint32_t frameNumber );
+
+  /**
+   * @copydoc Dali::VectorAnimationRenderer::GetTotalFrameNumber()
+   */
+  uint32_t GetTotalFrameNumber();
+
+private:
+
+  /**
+   * @brief Constructor
+   */
+  VectorAnimationRenderer();
+
+  /**
+   * @brief Destructor.
+   */
+  ~VectorAnimationRenderer();
+
+private:
+
+  VectorAnimationRenderer( const VectorAnimationRenderer& ) = delete;
+  VectorAnimationRenderer& operator=( VectorAnimationRenderer& )  = delete;
+
+private:
+
+  VectorAnimationRendererPluginProxy mPlugin;
+};
+
+} // namespace Adaptor
+
+} // namespace Internal
+
+inline static Internal::Adaptor::VectorAnimationRenderer& GetImplementation( Dali::VectorAnimationRenderer& renderer )
+{
+  DALI_ASSERT_ALWAYS( renderer && "VectorAnimationRenderer handle is empty." );
+
+  BaseObject& handle = renderer.GetBaseObject();
+
+  return static_cast< Internal::Adaptor::VectorAnimationRenderer& >( handle );
+}
+
+inline static const Internal::Adaptor::VectorAnimationRenderer& GetImplementation( const Dali::VectorAnimationRenderer& renderer )
+{
+  DALI_ASSERT_ALWAYS( renderer && "VectorAnimationRenderer handle is empty." );
+
+  const BaseObject& handle = renderer.GetBaseObject();
+
+  return static_cast< const Internal::Adaptor::VectorAnimationRenderer& >( handle );
+}
+
+} // namespace Dali
+
+#endif // DALI_INTERNAL_VECTOR_ANIMATION_RENDERER_IMPL_H
