4c5922f1e4bd4877cb53da05d9888d2cf4b5a231
David Steele <david.steele@samsung.com>
2018-12-06 20:56:20 +0900

    Signal Core when render thread is about to shutdown.
    Change-Id: Ib76bf41149b6f6473f96234003fe55b53ffbd913
    Signed-off-by: David Steele <david.steele@samsung.com>

--- dali/internal/adaptor/common/combined-update-render-controller.cpp
+++ dali/internal/adaptor/common/combined-update-render-controller.cpp
@@ -608,6 +608,7 @@ void CombinedUpdateRenderController::UpdateRenderThread()
     }
   }
 
+  mCore.GraphicsShutdown();
   currentSurface = mAdaptorInterfaces.GetRenderSurfaceInterface();
   if( currentSurface )
   {
