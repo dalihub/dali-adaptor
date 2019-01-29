ba146114b84154deec083fce1d68c7af044552b3
Wonsik Jung <sidein@samsung.com>
2019-01-29 14:47:53 +0900

    Revert "[Tizen] Add codes for Dali Windows Backend"
    This reverts commit ff8e2614c73c417c4cd9b20c071a4644a095f563.

--- dali/internal/imaging/common/file-download.h
+++ dali/internal/imaging/common/file-download.h
@@ -70,10 +70,6 @@ public:
    */
   static void GetThreadId( CRYPTO_THREADID* tid );
 
-  static void ConfigureCurlOptions( void* curlHandle, const std::string& url );
-
-  static void InitWriteFunction( void* curlHandle );
-
 private:
 
   void SetLockingFunction();
