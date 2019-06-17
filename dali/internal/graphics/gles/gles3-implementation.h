ddbe29b015cbed9fd34ff932875e3f407596e2eb
Jiyun Yang <ji.yang@samsung.com>
2019-06-17 14:59:59 +0900

    Revert "[Tizen] Coverity fix, Add virtual in the destructor of GlesAbstraction"
    This reverts commit c087a244556b2a4ea0eec6bbfd9b57fa4bc7a598.

--- dali/internal/graphics/gles/gles3-implementation.h
+++ dali/internal/graphics/gles/gles3-implementation.h
@@ -39,7 +39,7 @@ class Gles3Implementation : public GlesAbstraction
 public:
   Gles3Implementation() {}
 
-  ~Gles3Implementation() override {}
+  ~Gles3Implementation() {}
 
   void ReadBuffer( GLenum mode ) override
   {
