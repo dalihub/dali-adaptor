eef6cafea2259fd596e2cc5188d3a3854f0e12a0
Wonsik Jung <sidein@samsung.com>
2019-07-01 14:18:54 +0900

    Revert "[Tizen] Call RequestUpdateOnce in OnWindowShown()"
    This reverts commit 6198a46e7cc1a9aae80a5522f6a89a55ccec04df.

--- dali/internal/adaptor/common/adaptor-impl.cpp
+++ dali/internal/adaptor/common/adaptor-impl.cpp
@@ -847,9 +847,6 @@ void Adaptor::OnWindowShown()
   }
   else
   {
-    // Force a render task
-    RequestUpdateOnce();
-
     DALI_LOG_RELEASE_INFO( "Adaptor::OnWindowShown: Adaptor is not paused state.[%d]\n", mState );
   }
 }
