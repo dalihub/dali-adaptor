b0dc938fc5fd49e890d25ca2b34d9ff3183d529b
Anton Obzhirov <a.obzhirov@samsung.com>
2019-07-09 10:43:07 +0100

    Add file stream API.
    Change-Id: I8558863e04052c075f90135b3558f0d5ee1d2a12

---
+++ dali/devel-api/adaptor-framework/file-stream.cpp
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2019 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// CLASS HEADER
+#include <dali/devel-api/adaptor-framework/file-stream.h>
+
+// EXTERNAL INCLUDES
+
+// INTERNAL INCLUDES
+#include <dali/internal/adaptor-framework/common/file-stream-impl.h>
+
+namespace Dali
+{
+
+FileStream::FileStream(const std::string& filename, uint8_t mode)
+{
+  mImpl.reset( new Impl( filename, mode ) );
+}
+
+FileStream::FileStream(uint8_t* buffer, size_t dataSize, uint8_t mode)
+{
+  mImpl.reset( new Impl( buffer, dataSize, mode ) );
+}
+
+FileStream::FileStream(Dali::Vector<uint8_t>& buffer, size_t dataSize, uint8_t mode)
+{
+  mImpl.reset( new Impl( buffer, dataSize, mode ) );
+}
+
+FileStream::~FileStream() = default;
+
+FileStream& FileStream::operator=(FileStream&&) = default;
+
+std::iostream& FileStream::GetStream()
+{
+  return mImpl->GetStream();
+}
+
+FILE* FileStream::GetFile()
+{
+  return mImpl->GetFile();
+}
+
+} // Dali
