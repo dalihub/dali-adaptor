c909ab6047694ab91c1aad35608db216c1940240
Wonsik Jung <sidein@samsung.com>
2019-08-28 09:32:15 +0900

    Revert "[Tizen] To get MaxTextureSize by using glGetIntegerv not by using environment variable"
    This reverts commit d32e2ca1a0b91be571c983e12eda3f5b7b742e00.

--- dali/internal/imaging/common/image-loader.cpp
+++ dali/internal/imaging/common/image-loader.cpp
@@ -47,8 +47,6 @@ Integration::Log::Filter* gLogFilter = Debug::Filter::New( Debug::Concise, false
 
 static unsigned int gMaxTextureSize = 4096;
 
-static bool gMaxTextureSizeUpdated = false;
-
 /**
  * Enum for file formats, has to be in sync with BITMAP_LOADER_LOOKUP_TABLE
  */
@@ -420,17 +418,11 @@ ImageDimensions GetClosestImageSize( Integration::ResourcePointer resourceBuffer
 void SetMaxTextureSize( unsigned int size )
 {
   gMaxTextureSize = size;
-  gMaxTextureSizeUpdated = true;
 }
 
 unsigned int GetMaxTextureSize()
 {
   return gMaxTextureSize;
-}
-
-bool MaxTextureSizeUpdated()
-{
-  return gMaxTextureSizeUpdated;
 }
 
 } // ImageLoader
