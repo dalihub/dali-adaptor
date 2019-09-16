a61186ebb4fc7b72d2fa995cdb6eb2e7c1afa02b
Joogab Yun <joogab.yun@samsung.com>
2019-09-16 15:45:56 +0900

    [Tizen] Implement partial update
    This reverts commit bb80b039c298d7ab0179a2842c813d37347ed373.
    
    Change-Id: Ie9b9943983ff119df5c0cf71520c815720e86528
    Signed-off-by: huiyu.eun <huiyu.eun@samsung.com>

--- dali/internal/window-system/tizen-wayland/native-render-surface-ecore-wl.h
+++ dali/internal/window-system/tizen-wayland/native-render-surface-ecore-wl.h
@@ -159,6 +159,18 @@ public: // from Dali::Integration::RenderSurface
    */
   virtual Integration::StencilBufferAvailable GetStencilBufferRequired() override;
 
+  /**
+   * @copydoc Dali::Integration::RenderSurface::SetDamagedRect()
+   */
+  virtual Rect<int32_t> SetDamagedRect( const Rect<int32_t>& damagedRect ) override;
+
+  /**
+   * @copydoc Dali::Integration::RenderSurface::GetBufferAge()
+   */
+  virtual int32_t GetBufferAge() override;
+
+
+
 private:
 
   /**
