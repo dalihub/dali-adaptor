819c4738b2f54c49e0299f7ff04d9b46e817aa78
Seoyeon Kim <seoyeon2.kim@samsung.com>
2019-10-08 14:26:48 +0900

    [Tizen] Add screen and client rotation itself function
    This reverts commit c3916fe1fb10465161a98b77d2bc92786f10e4eb.

--- dali/internal/window-system/windows/platform-implement-win.h
+++ dali/internal/window-system/windows/platform-implement-win.h
@@ -77,6 +77,8 @@ void KillTimer(int id);
 
 void GetDPI( uint64_t hWnd, float &xDpi, float &yDpi );
 
+int GetOrientation() const;
+
 const char* GetKeyName( int keyCode );
 
 uint64_t GetCurrentThreadId();
