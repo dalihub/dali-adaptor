6c88dafb3cd80ef73a39e9dfbe6d1be17ce5d303
Heeyong Song <heeyong.song@samsung.com>
2019-12-12 14:55:15 +0900

    (Vector) GetMarkerInfo method
    Change-Id: Ic3b29173e7a19bd10e0f0b77ceba1801c0564478

--- dali/devel-api/adaptor-framework/vector-animation-renderer-plugin.h
+++ dali/devel-api/adaptor-framework/vector-animation-renderer-plugin.h
@@ -114,6 +114,21 @@ public:
   virtual void GetLayerInfo( Property::Map& map ) const = 0;
 
   /**
+   * @brief Gets the start frame and the end frame number of the composition marker.
+   *
+   * @param[in] marker The composition marker of the file
+   * @param[out] startFrame The start frame number of the specified marker
+   * @param[out] endFrame The end frame number of the specified marker
+   * @return True if the marker is found in the file, false otherwise.
+   *
+   * @note https://helpx.adobe.com/after-effects/using/layer-markers-composition-markers.html
+   * Markers exported from AfterEffect are used to describe a segment of an animation {comment/tag , startFrame, endFrame}
+   * Marker can be use to devide a resource in to separate animations by tagging the segment with comment string,
+   * start frame and duration of that segment.
+   */
+  virtual bool GetMarkerInfo( const std::string& marker, uint32_t& startFrame, uint32_t& endFrame ) const = 0;
+
+  /**
    * @brief Connect to this signal to be notified when the texture upload is completed.
    *
    * @return The signal to connect to.
