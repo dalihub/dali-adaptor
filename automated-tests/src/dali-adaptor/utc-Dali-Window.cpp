bd4ad86b80eef2cb740013c05446934fcf114f87
Adeel Kazmi <adeel.kazmi@samsung.com>
2020-01-07 16:15:58 +0000

    [Tizen] GCC 9.2 Support
    Change-Id: I3ae4c6c43967dc12691095135c21fc3a0ad4366f

--- automated-tests/src/dali-adaptor/utc-Dali-Window.cpp
+++ automated-tests/src/dali-adaptor/utc-Dali-Window.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019 Samsung Electronics Co., Ltd.
+ * Copyright (c) 2020 Samsung Electronics Co., Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,7 +16,7 @@
  */
 
 #include <dali/dali.h>
-#include <Ecore_X.h>
+#include <dali/internal/system/linux/dali-ecore-x.h>
 #include <dali-test-suite-utils.h>
 
 namespace Dali
