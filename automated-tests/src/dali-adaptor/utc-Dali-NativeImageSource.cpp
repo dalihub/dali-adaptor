95451206d2b67f4d42e50f8ff62f666b7d03fd50
Seungho, Baek <sbsh.baek@samsung.com>
2020-01-21 17:33:08 +0900

    Revert "[Tizen] GCC 9.2 Support"
    This reverts commit bd4ad86b80eef2cb740013c05446934fcf114f87.

--- automated-tests/src/dali-adaptor/utc-Dali-NativeImageSource.cpp
+++ automated-tests/src/dali-adaptor/utc-Dali-NativeImageSource.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020 Samsung Electronics Co., Ltd.
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,6 +17,8 @@
 
 #include <dali/dali.h>
 #include <dali-test-suite-utils.h>
+
+#include <Ecore_X.h>
 
 using namespace Dali;
 
--- automated-tests/src/dali-adaptor/utc-Dali-Timer.cpp
+++ automated-tests/src/dali-adaptor/utc-Dali-Timer.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020 Samsung Electronics Co., Ltd.
+ * Copyright (c) 2018 Samsung Electronics Co., Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,7 +19,7 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <dali/dali.h>
-#include <dali/internal/system/linux/dali-ecore.h>
+#include <Ecore.h>
 #include <dali-test-suite-utils.h>
 #include <adaptor-test-application.h>
 
