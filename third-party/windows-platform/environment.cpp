7ad5e5732b3187023687f7fed19b96dc93b57978
Victor Cebollada <v.cebollada@samsung.com>
2020-02-27 07:55:18 +0000

    Removed the 'setenv' function of the Third Party Windows platform.
    * Moved to windows-dependencies as it's needed by dali-demo.
    
    Change-Id: I329519c6825f4f9487cd05ded5f085afc6481bac
    Signed-off-by: Victor Cebollada <v.cebollada@samsung.com>

--- third-party/windows-platform/environment.cpp
+++ third-party/windows-platform/environment.cpp
@@ -3,15 +3,6 @@
 
 using namespace std;
 
-int setenv( const char *__name, const char *__value, int __replace )
-{
-  string value = __name;
-  value += "=";
-  value += __value;
-
-  return putenv( value.c_str() );
-}
-
 const char* app_get_data_path()
 {
   static std::string envValue = "";
