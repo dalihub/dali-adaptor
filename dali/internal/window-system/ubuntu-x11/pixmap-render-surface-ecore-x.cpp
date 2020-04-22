ae6dc30882f7a881bde477d6120bfe62360b4742
Jiyun Yang <ji.yang@samsung.com>
2020-04-22 15:07:24 +0900

    Revert "[Tizen] Add screen and client rotation itself function"
    This reverts commit 3a3fbfe13a7b09dfa2fbfa8cabf5a70fb91dd04c.

--- dali/internal/window-system/ubuntu-x11/pixmap-render-surface-ecore-x.cpp
+++ dali/internal/window-system/ubuntu-x11/pixmap-render-surface-ecore-x.cpp
@@ -147,11 +147,6 @@ void PixmapRenderSurfaceEcoreX::GetDpi( unsigned int& dpiHorizontal, unsigned in
   dpiVertical   = int( yres + 0.5f );
 }
 
-int PixmapRenderSurfaceEcoreX::GetOrientation() const
-{
-  return 0;
-}
-
 void PixmapRenderSurfaceEcoreX::InitializeGraphics()
 {
   mGraphics = &mAdaptor->GetGraphicsInterface();
