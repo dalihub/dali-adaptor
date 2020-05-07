94c641cee7e33a291d709283ee667953350ddae4
Wonsik Jung <sidein@samsung.com>
2020-05-07 19:36:59 +0900

    [Tizen] Fix screen rotation bug when application is initialized.
    When DALi application initialized, DALi checks the value of screen rotation.
    After that, DALi does not work the screen rotation's process.
    To fix, the valid output transform event is used the init time.
    
    Change-Id: I31906c3ac014cd21e453e477599e6dbd8a874f43

--- dali/internal/window-system/common/window-impl.cpp
+++ dali/internal/window-system/common/window-impl.cpp
@@ -739,6 +739,7 @@ void Window::OnOutputTransformed()
   {
     forceUpdate = true;
   }
+  DALI_LOG_RELEASE_INFO( "Window (%p), WinId (%d), OnOutputTransformed()\n", this, mNativeWindowId );
   SurfaceResized( forceUpdate );
 
   PositionSize positionSize = mSurface->GetPositionSize();
