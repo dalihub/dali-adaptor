3c189916904a3c0798fbf7c770eb51afddf9daf4
Wonsik Jung <sidein@samsung.com>
2020-06-02 11:31:57 +0900

    Revert "[Tizen] Call PostRender method of the surface if needed"
    This reverts commit ab707e9ca7437d4aa06353e97ee716c25e512495.

--- dali/internal/adaptor/common/combined-update-render-controller.cpp
+++ dali/internal/adaptor/common/combined-update-render-controller.cpp
@@ -713,10 +713,7 @@ void CombinedUpdateRenderController::UpdateRenderThread()
           // Render the surface
           mCore.RenderScene( scene, false );
 
-          if( renderStatus.NeedsPostRender() )
-          {
-            windowSurface->PostRender( false, false, surfaceResized ); // Swap Buffer
-          }
+          windowSurface->PostRender( false, false, surfaceResized ); // Swap Buffer
         }
       }
     }
