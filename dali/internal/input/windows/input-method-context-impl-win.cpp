ec76e50dd21888a0049b003df0c2d3b4b6a257de
Victor Cebollada <v.cebollada@samsung.com>
2020-08-26 09:42:06 +0100

    Fix build errors for MS-Windows after KeyEvent class pimpling patch.
    Change-Id: I14db2cfdda393b034251a32d69abb27e091931c9
    Signed-off-by: Victor Cebollada <v.cebollada@samsung.com>

--- dali/internal/input/windows/input-method-context-impl-win.cpp
+++ dali/internal/input/windows/input-method-context-impl-win.cpp
@@ -342,11 +342,11 @@ bool InputMethodContextWin::FilterEventKey( const Dali::KeyEvent& keyEvent )
   if ( ! KeyLookup::IsDeviceButton( keyEvent.GetKeyName().c_str() ))
   {
     //check whether it's key down or key up event
-    if ( keyEvent.state == Dali::KeyEvent::Down )
+    if ( keyEvent.GetState() == Dali::KeyEvent::Down )
     {
       eventHandled = ProcessEventKeyDown( keyEvent );
     }
-    else if ( keyEvent.state == Dali::KeyEvent::Up )
+    else if ( keyEvent.GetState() == Dali::KeyEvent::Up )
     {
       eventHandled = ProcessEventKeyUp( keyEvent );
     }
@@ -377,13 +377,13 @@ void InputMethodContextWin::GetPreeditStyle( Dali::InputMethodContext::PreEditAt
   attrs = mPreeditAttrs;
 }
 
-bool InputMethodContextWin::ProcessEventKeyDown( const KeyEvent& keyEvent )
+bool InputMethodContextWin::ProcessEventKeyDown( const Dali::KeyEvent& keyEvent )
 {
   bool eventHandled( false );
   return eventHandled;
 }
 
-bool InputMethodContextWin::ProcessEventKeyUp( const KeyEvent& keyEvent )
+bool InputMethodContextWin::ProcessEventKeyUp( const Dali::KeyEvent& keyEvent )
 {
   bool eventHandled( false );
   return eventHandled;
