dabfa49a4c6c1d289c03b283dd76bdbdf9f04a00
Joogab Yun <joogab.yun@samsung.com>
2020-09-07 14:39:06 +0900

    Revert "Revert "Renamed KeyEvent enum values to comply with coding standards.""
    This reverts commit 70d9fd825bad24b44f246aa24f7a3d317715737b.

--- dali/internal/input/windows/input-method-context-impl-win.cpp
+++ dali/internal/input/windows/input-method-context-impl-win.cpp
@@ -342,11 +342,11 @@ bool InputMethodContextWin::FilterEventKey( const Dali::KeyEvent& keyEvent )
   if ( ! KeyLookup::IsDeviceButton( keyEvent.GetKeyName().c_str() ))
   {
     //check whether it's key down or key up event
-    if ( keyEvent.GetState() == Dali::KeyEvent::Down )
+    if ( keyEvent.GetState() == Dali::KeyEvent::DOWN )
     {
       eventHandled = ProcessEventKeyDown( keyEvent );
     }
-    else if ( keyEvent.GetState() == Dali::KeyEvent::Up )
+    else if ( keyEvent.GetState() == Dali::KeyEvent::UP )
     {
       eventHandled = ProcessEventKeyUp( keyEvent );
     }
