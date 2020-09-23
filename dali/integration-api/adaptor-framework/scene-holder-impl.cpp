fe649671feabf0fd2f09fdfab62e3b2bf52f89ff
Sunghyun Kim <scholb.kim@samsung.com>
2020-09-23 11:08:09 +0900

    [Tizen] Add screen and client rotation itself function
    This reverts commit fb301c1dd3260b09cf0fe34f01068e3dc20a0ca2.
    
    Change-Id: I575e15240b9f4527219b4d6d7b3c5cad72a359cc

--- dali/integration-api/adaptor-framework/scene-holder-impl.cpp
+++ dali/integration-api/adaptor-framework/scene-holder-impl.cpp
@@ -190,7 +190,7 @@ void SceneHolder::SetSurface(Dali::RenderSurfaceInterface* surface)
 
   mScene.SurfaceReplaced();
 
-  SurfaceResized();
+  SurfaceResized(false);
 
   InitializeDpi();
 
@@ -200,10 +200,11 @@ void SceneHolder::SetSurface(Dali::RenderSurfaceInterface* surface)
   OnSurfaceSet(surface);
 }
 
-void SceneHolder::SurfaceResized()
+void SceneHolder::SurfaceResized(bool forceUpdate)
 {
   PositionSize surfacePositionSize = mSurface->GetPositionSize();
-  mScene.SurfaceResized(static_cast<float>(surfacePositionSize.width), static_cast<float>(surfacePositionSize.height));
+  int          orientation         = mSurface->GetOrientation();
+  mScene.SurfaceResized(static_cast<float>(surfacePositionSize.width), static_cast<float>(surfacePositionSize.height), orientation, forceUpdate);
 
   mSurface->SetFullSwapNextFrame();
 }
