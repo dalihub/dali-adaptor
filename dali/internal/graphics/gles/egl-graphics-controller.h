b2e4db87f56a6d06dd880ec888b3f1314333add3
adam.b <adam.b@samsung.com>
2021-01-21 12:43:29 +0000

    Dummy graphics controller
    Change-Id: I23618deab26e4e8f1ef59bd09d86f8f423426593

--- dali/internal/graphics/gles/egl-graphics-controller.h
+++ dali/internal/graphics/gles/egl-graphics-controller.h
@@ -35,12 +35,12 @@ namespace Graphics
  *
  * Temporarily holds the old GL abstractions whilst dali-core is migrated to the new API.
  */
-class EglGraphicsController : public Graphics::Controller
+DALI_IMPORT_API class EglGraphicsController : public Graphics::Controller
 {
 public:
   EglGraphicsController() = default;
 
-  virtual ~EglGraphicsController() = default;
+  ~EglGraphicsController() override = default;
 
   /**
    * Initialize the GLES abstraction. This can be called from the main thread.
@@ -58,6 +58,220 @@ public:
   Integration::GlAbstraction&              GetGlAbstraction() override;
   Integration::GlSyncAbstraction&          GetGlSyncAbstraction() override;
   Integration::GlContextHelperAbstraction& GetGlContextHelperAbstraction() override;
+
+  /**
+   * @copydoc Dali::Graphics::SubmitCommandBuffers()
+   */
+  void SubmitCommandBuffers(const SubmitInfo& submitInfo) override
+  {
+  }
+
+  /**
+   * @copydoc Dali::Graphics::PresentRenderTarget()
+   */
+  void PresentRenderTarget(RenderTarget* renderTarget) override
+  {
+  }
+
+  /**
+   * @copydoc Dali::Graphics::WaitIdle()
+   */
+  void WaitIdle() override
+  {
+  }
+
+  /**
+   * @copydoc Dali::Graphics::Pause()
+   */
+  void Pause() override
+  {
+  }
+
+  /**
+   * @copydoc Dali::Graphics::Resume()
+   */
+  void Resume() override
+  {
+  }
+
+  /**
+   * @copydoc Dali::Graphics::UpdateTextures()
+   */
+  void UpdateTextures(const std::vector<TextureUpdateInfo>&       updateInfoList,
+                      const std::vector<TextureUpdateSourceInfo>& sourceList) override
+  {
+  }
+
+  /**
+   * @copydoc Dali::Graphics::EnableDepthStencilBuffer()
+   */
+  bool EnableDepthStencilBuffer(bool enableDepth, bool enableStencil) override
+  {
+    return {};
+  }
+
+  /**
+   * @copydoc Dali::Graphics::RunGarbageCollector()
+   */
+  void RunGarbageCollector(size_t numberOfDiscardedRenderers) override
+  {
+  }
+
+  /**
+   * @copydoc Dali::Graphics::DiscardUnusedResources()
+   */
+  void DiscardUnusedResources() override
+  {
+  }
+
+  /**
+   * @copydoc Dali::Graphics::IsDiscardQueueEmpty()
+   */
+  bool IsDiscardQueueEmpty() override
+  {
+    return {};
+  }
+
+  /**
+   * @copydoc Dali::Graphics::IsDrawOnResumeRequired()
+   */
+  bool IsDrawOnResumeRequired() override
+  {
+    return {};
+  }
+
+  /**
+   * @copydoc Dali::Graphics::CreateBuffer()
+   */
+  std::unique_ptr<Buffer> CreateBuffer(const BufferCreateInfo& bufferCreateInfo, std::unique_ptr<Buffer>&& oldBuffer) override
+  {
+    return nullptr;
+  }
+
+  /**
+   * @copydoc Dali::Graphics::CreateCommandBuffer()
+   */
+  std::unique_ptr<CommandBuffer> CreateCommandBuffer(const CommandBufferCreateInfo& commandBufferCreateInfo, std::unique_ptr<CommandBuffer>&& oldCommandBuffer) override
+  {
+    return nullptr;
+  }
+
+  /**
+   * @copydoc Dali::Graphics::CreateRenderPass()
+   */
+  std::unique_ptr<RenderPass> CreateRenderPass(const RenderPassCreateInfo& renderPassCreateInfo, std::unique_ptr<RenderPass>&& oldRenderPass) override
+  {
+    return nullptr;
+  }
+
+  /**
+   * @copydoc Dali::Graphics::CreateTexture()
+   */
+  std::unique_ptr<Texture> CreateTexture(const TextureCreateInfo& textureCreateInfo, std::unique_ptr<Texture>&& oldTexture) override
+  {
+    return nullptr;
+  }
+
+  /**
+   * @copydoc Dali::Graphics::CreateFramebuffer()
+   */
+  std::unique_ptr<Framebuffer> CreateFramebuffer(const FramebufferCreateInfo& framebufferCreateInfo, std::unique_ptr<Framebuffer>&& oldFramebuffer) override
+  {
+    return nullptr;
+  }
+
+  /**
+   * @copydoc Dali::Graphics::CreatePipeline()
+   */
+  std::unique_ptr<Pipeline> CreatePipeline(const PipelineCreateInfo& pipelineCreateInfo, std::unique_ptr<Pipeline>&& oldPipeline) override
+  {
+    return nullptr;
+  }
+
+  /**
+   * @copydoc Dali::Graphics::CreateShader()
+   */
+  std::unique_ptr<Shader> CreateShader(const ShaderCreateInfo& shaderCreateInfo, std::unique_ptr<Shader>&& oldShader) override
+  {
+    return nullptr;
+  }
+
+  /**
+   * @copydoc Dali::Graphics::CreateSampler()
+   */
+  std::unique_ptr<Sampler> CreateSampler(const SamplerCreateInfo& samplerCreateInfo, std::unique_ptr<Sampler>&& oldSampler) override
+  {
+    return nullptr;
+  }
+
+  /**
+   * @copydoc Dali::Graphics::CreateRenderTarget()
+   */
+  std::unique_ptr<RenderTarget> CreateRenderTarget(const RenderTargetCreateInfo& renderTargetCreateInfo, std::unique_ptr<RenderTarget>&& oldRenderTarget) override
+  {
+    return nullptr;
+  }
+
+  /**
+   * @copydoc Dali::Graphics::MapBufferRange()
+   */
+  std::unique_ptr<Memory> MapBufferRange(const MapBufferInfo& mapInfo) override
+  {
+    return nullptr;
+  }
+
+  /**
+   * @copydoc Dali::Graphics::MapTextureRange()
+   */
+  std::unique_ptr<Memory> MapTextureRange(const MapTextureInfo& mapInfo) override
+  {
+    return nullptr;
+  }
+
+  /**
+   * @copydoc Dali::Graphics::UnmapMemory()
+   */
+  void UnmapMemory(std::unique_ptr<Memory> memory) override
+  {
+  }
+  /**
+   * @copydoc Dali::Graphics::GetTextureMemoryRequirements()
+   */
+  MemoryRequirements GetTextureMemoryRequirements(Texture& texture) const override
+  {
+    return {};
+  }
+
+  /**
+   * @copydoc Dali::Graphics::GetBufferMemoryRequirements()
+   */
+  MemoryRequirements GetBufferMemoryRequirements(Buffer& buffer) const override
+  {
+    return {};
+  }
+
+  /**
+   * @copydoc Dali::Graphics::GetTextureProperties()
+   */
+  const TextureProperties& GetTextureProperties(const Texture& texture) override
+  {
+    // for compiler not to moan
+    static TextureProperties dummy{};
+    return dummy;
+  }
+
+  /**
+   * @copydoc Dali::Graphics::PipelineEquals()
+   */
+  [[nodiscard]] bool PipelineEquals(const Pipeline& pipeline0, const Pipeline& pipeline1) const override
+  {
+    return {};
+  }
+
+  [[nodiscard]] Integration::GlAbstraction* GetGL() const
+  {
+    return mGlAbstraction;
+  }
 
 private:
   Integration::GlAbstraction*              mGlAbstraction{nullptr};
