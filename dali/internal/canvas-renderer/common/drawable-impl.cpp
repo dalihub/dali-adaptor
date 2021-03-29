20de94bd455ffa5a0211d8797de4f455ef598f02
JunsuChoi <jsuya.choi@samsung.com>
2021-03-29 15:16:21 +0900

    [Tizen] Implements CanvasRenderer that can draw Vector Primitives using ThorVG
    This reverts commit a1fdcba71e41d25f789a468efa32fbea6d2aa023.

---
+++ dali/internal/canvas-renderer/common/drawable-impl.cpp
@@ -0,0 +1,154 @@
+/*
+ * Copyright (c) 2021 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+// CLASS HEADER
+#include <dali/internal/canvas-renderer/common/drawable-factory.h>
+#include <dali/internal/canvas-renderer/common/drawable-impl.h>
+
+namespace Dali
+{
+namespace Internal
+{
+namespace Adaptor
+{
+Drawable::Drawable() = default;
+
+Drawable::~Drawable()
+{
+  if(pImpl)
+  {
+    delete pImpl;
+  }
+}
+
+void Drawable::Create()
+{
+  if(!pImpl)
+  {
+    pImpl = Internal::Adaptor::DrawableFactory::New();
+  }
+}
+
+bool Drawable::SetOpacity(float opacity)
+{
+  if(!pImpl)
+  {
+    return false;
+  }
+  return pImpl->SetOpacity(opacity);
+}
+
+float Drawable::GetOpacity() const
+{
+  if(!pImpl)
+  {
+    return 0.0f;
+  }
+  return pImpl->GetOpacity();
+}
+
+bool Drawable::Rotate(Degree degree)
+{
+  if(!pImpl)
+  {
+    return false;
+  }
+  return pImpl->Rotate(degree);
+}
+
+bool Drawable::Scale(float factor)
+{
+  if(!pImpl)
+  {
+    return false;
+  }
+  return pImpl->Scale(factor);
+}
+
+bool Drawable::Translate(Vector2 translate)
+{
+  if(!pImpl)
+  {
+    return false;
+  }
+  return pImpl->Translate(translate);
+}
+
+bool Drawable::Transform(const Dali::Matrix3& matrix)
+{
+  if(!pImpl)
+  {
+    return false;
+  }
+  return pImpl->Transform(matrix);
+}
+
+void Drawable::SetDrawableAdded(bool added)
+{
+  if(!pImpl)
+  {
+    return;
+  }
+  pImpl->SetDrawableAdded(added);
+}
+
+void* Drawable::GetObject() const
+{
+  if(!pImpl)
+  {
+    return nullptr;
+  }
+  return pImpl->GetObject();
+}
+
+void Drawable::SetObject(const void* object)
+{
+  if(!pImpl)
+  {
+    return;
+  }
+  pImpl->SetObject(object);
+}
+
+void Drawable::SetChanged(bool changed)
+{
+  if(!pImpl)
+  {
+    return;
+  }
+  pImpl->SetChanged(changed);
+}
+
+bool Drawable::GetChanged() const
+{
+  if(!pImpl)
+  {
+    return false;
+  }
+  return pImpl->GetChanged();
+}
+
+Dali::Internal::Adaptor::Drawable* Drawable::GetImplementation()
+{
+  return pImpl;
+}
+
+} // namespace Adaptor
+
+} // namespace Internal
+
+} // namespace Dali
