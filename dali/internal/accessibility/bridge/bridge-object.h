af393bf8a62bbe38993da9ab11c8e4b8e9bcc7dd
Shinwoo Kim <cinoo.kim@samsung.com>
2021-04-06 17:47:32 +0900

    atspi: remove undefined method
    This patch is removing undefined methods in BridgeObject.
    Those methods are defined in BridgeAccessible.
    
    Change-Id: I7fb931150b489afd848a41869a5b4a15405ef55c

--- dali/internal/accessibility/bridge/bridge-object.h
+++ dali/internal/accessibility/bridge/bridge-object.h
@@ -44,21 +44,6 @@ protected:
   void Emit(Dali::Accessibility::Accessible* obj, Dali::Accessibility::WindowEvent we, unsigned int detail1) override;
   void Emit(Dali::Accessibility::Accessible* obj, Dali::Accessibility::ObjectPropertyChangeEvent we) override;
   void EmitBoundsChanged(Dali::Accessibility::Accessible* obj, Dali::Rect<> rect) override;
-
-public:
-  int                                                                GetChildCount();
-  DBus::ValueOrError<Dali::Accessibility::Accessible*>               GetChildAtIndex(int index);
-  Dali::Accessibility::Accessible*                                   GetParent();
-  DBus::ValueOrError<std::vector<Dali::Accessibility::Accessible*> > GetChildren();
-  std::string                                                        GetName();
-  std::string                                                        GetDescription();
-  DBus::ValueOrError<uint32_t>                                       GetRole();
-  DBus::ValueOrError<std::string>                                    GetRoleName();
-  DBus::ValueOrError<std::string>                                    GetLocalizedRoleName();
-  DBus::ValueOrError<int32_t>                                        GetIndexInParent();
-  DBus::ValueOrError<std::array<uint32_t, 2> >                       GetStates();
-  DBus::ValueOrError<std::unordered_map<std::string, std::string> >  GetAttributes();
-  DBus::ValueOrError<std::vector<std::string> >                      GetInterfaces();
 };
 
 #endif // DALI_INTERNAL_ACCESSIBILITY_BRIDGE_OBJECT_H
