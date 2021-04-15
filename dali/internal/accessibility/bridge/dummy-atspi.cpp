4c0578579708a1da67df9b3fc857704029015fc3
Artur Świgoń <a.swigon@samsung.com>
2021-04-15 11:53:25 +0200

    [AT-SPI] Add API for blocking automatic Bridge initialization
    Change-Id: Iee34ae36bc5778e610a01da5b96e2ecf5408bcf9

--- dali/internal/accessibility/bridge/dummy-atspi.cpp
+++ dali/internal/accessibility/bridge/dummy-atspi.cpp
@@ -75,6 +75,14 @@ Accessibility::Bridge* Accessibility::Bridge::GetCurrentBridge()
   return Accessibility::DummyBridge::GetInstance();
 }
 
+void Accessibility::Bridge::DisableAutoInit()
+{
+}
+
+void Accessibility::Bridge::EnableAutoInit(Accessible*, const std::string&)
+{
+}
+
 void Accessibility::Accessible::EmitStateChanged(Accessibility::State state, int newValue1, int newValue2)
 {
 }
