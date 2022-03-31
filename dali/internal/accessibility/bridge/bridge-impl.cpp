74834c6a17f4fa7e01610a33107e8591d1586836
Shinwoo Kim <cinoo.kim@samsung.com>
2022-03-31 20:02:02 +0900

    [AT-SPI] do not keep window in ApplicationAccessible
    There is not a reason to keep window in ApplicationAccessible to emit
    window related signals.
    
    Moreover, keeping window increases the reference count.
    In multi window case, Window.Reset() for the second window
    cannot make ~Window() is called because of the reference count is not 0.
    
    Change-Id: I13f636b12c62a29dc976602446b5dc13f2b521cb

--- dali/internal/accessibility/bridge/bridge-impl.cpp
+++ dali/internal/accessibility/bridge/bridge-impl.cpp
@@ -235,7 +235,6 @@ public:
     mDirectReadingClient  = {};
     mDirectReadingCallbacks.clear();
     mApplication.mChildren.clear();
-    mApplication.mWindows.clear();
     ClearTimer();
   }
 
@@ -359,13 +358,6 @@ public:
     mApplication.mParent.SetAddress(std::move(std::get<0>(res)));
 
     mEnabledSignal.Emit();
-
-    if(mIsShown)
-    {
-      auto rootLayer = Dali::Stage::GetCurrent().GetRootLayer();
-      auto window    = Dali::DevelWindow::Get(rootLayer);
-      EmitActivate(window); // Currently, sends a signal that the default window is activated here.
-    }
 
     return ForceUpResult::JUST_STARTED;
   }
