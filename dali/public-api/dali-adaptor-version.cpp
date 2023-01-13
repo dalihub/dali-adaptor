a5f0c4eb5527c58a266aa4cbd0cd6d43bb1b1723
Adeel Kazmi <adeel.kazmi@samsung.com>
2023-01-13 09:56:10 +0000

    DALi Version 2.2.9
    Change-Id: Iead2f576c2507d62e92b79cee286abd4797ce263

--- dali/public-api/dali-adaptor-version.cpp
+++ dali/public-api/dali-adaptor-version.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 Samsung Electronics Co., Ltd.
+ * Copyright (c) 2023 Samsung Electronics Co., Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -27,7 +27,7 @@ namespace Dali
 {
 const unsigned int ADAPTOR_MAJOR_VERSION = 2;
 const unsigned int ADAPTOR_MINOR_VERSION = 2;
-const unsigned int ADAPTOR_MICRO_VERSION = 8;
+const unsigned int ADAPTOR_MICRO_VERSION = 9;
 const char* const  ADAPTOR_BUILD_DATE    = __DATE__ " " __TIME__;
 
 #ifdef DEBUG_ENABLED
