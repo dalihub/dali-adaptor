825d253b08eb3298ff252f8db4b3f0a5eb0b39b4
Woochan Lee <wc0917.lee@samsung.com>
2024-08-07 14:12:25 +0900

    [Tizen] Add WebView APIs for fullscreen, text found, etc.
    This reverts commit 99a4fe615f974236b8b8363f1ef5a207777010b1.

--- dali/devel-api/adaptor-framework/web-engine/web-engine.h
+++ dali/devel-api/adaptor-framework/web-engine/web-engine.h
@@ -662,6 +662,11 @@ public:
   bool SendWheelEvent(const WheelEvent& event);
 
   /**
+   * @brief Exit full-screen.
+   */
+  void ExitFullscreen();
+
+  /**
    * @brief Callback to be called when frame would be rendered.
    *
    * @param[in] callback
@@ -739,6 +744,13 @@ public:
   void RegisterNavigationPolicyDecidedCallback(Dali::WebEnginePlugin::WebEngineNavigationPolicyDecidedCallback callback);
 
   /**
+   * @brief Callback to be called when new window policy would be decided.
+   *
+   * @param[in] callback
+   */
+  void RegisterNewWindowPolicyDecidedCallback(Dali::WebEnginePlugin::WebEngineNewWindowPolicyDecidedCallback callback);
+
+  /**
    * @brief Callback to be called when a new window would be created.
    *
    * @param[in] callback
@@ -779,6 +791,27 @@ public:
    * @param[in] callback
    */
   void RegisterContextMenuHiddenCallback(Dali::WebEnginePlugin::WebEngineContextMenuHiddenCallback callback);
+
+  /**
+   * @brief Callback to be called when fullscreen would be entered.
+   *
+   * @param[in] callback
+   */
+  void RegisterFullscreenEnteredCallback(Dali::WebEnginePlugin::WebEngineFullscreenEnteredCallback callback);
+
+  /**
+   * @brief Callback to be called when fullscreen would be exited.
+   *
+   * @param[in] callback
+   */
+  void RegisterFullscreenExitedCallback(Dali::WebEnginePlugin::WebEngineFullscreenExitedCallback callback);
+
+  /**
+   * @brief Callback to be called when text would be found.
+   *
+   * @param[in] callback
+   */
+  void RegisterTextFoundCallback(Dali::WebEnginePlugin::WebEngineTextFoundCallback callback);
 
   /**
    * @brief Get a plain text of current web page asynchronously.
