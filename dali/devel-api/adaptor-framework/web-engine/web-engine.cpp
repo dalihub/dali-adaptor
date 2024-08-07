2b092f0f720c9846726209109be405252c94ee17
Woochan Lee <wc0917.lee@samsung.com>
2024-08-07 14:08:36 +0900

    Revert "[Tizen] Add a WebView API for notifying orientation change."
    This reverts commit 8b320f3cfe0dc8cf34ae553dd09a8b8bfb06f6df.

--- dali/devel-api/adaptor-framework/web-engine/web-engine.cpp
+++ dali/devel-api/adaptor-framework/web-engine/web-engine.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 Samsung Electronics Co., Ltd.
+ * Copyright (c) 2023 Samsung Electronics Co., Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -103,11 +103,6 @@ Dali::WebEnginePlugin* WebEngine::GetPlugin() const
 NativeImageSourcePtr WebEngine::GetNativeImageSource()
 {
   return GetImplementation(*this).GetNativeImageSource();
-}
-
-void WebEngine::ChangeOrientation(int orientation)
-{
-  return GetImplementation(*this).ChangeOrientation(orientation);
 }
 
 Dali::WebEngineSettings& WebEngine::GetSettings() const
