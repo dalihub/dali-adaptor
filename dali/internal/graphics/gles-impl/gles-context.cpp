5f101994302362e13f834ab66d891cc7fb1be3d5
Eunki, Hong <eunkiki.hong@samsung.com>
2025-02-24 20:20:49 +0900

    [Tizen] Do not call glInvalidateFramebuffer for DEPTH only TV profile
    Change-Id: Id7acc34b8b7339c2cf9580c2960a2f475c8da1f7
    Signed-off-by: Eunki, Hong <eunkiki.hong@samsung.com>

--- dali/internal/graphics/gles-impl/gles-context.cpp
+++ dali/internal/graphics/gles-impl/gles-context.cpp
@@ -1005,17 +1005,15 @@ void Context::ClearBuffer(uint32_t mask, bool forceClear)
 
 void Context::InvalidateDepthStencilBuffers()
 {
+#ifndef DALI_PROFILE_TV
   if(auto* gl = mImpl->GetGL())
   {
-#ifndef DALI_PROFILE_TV
     GLenum attachments[] = {GL_DEPTH, GL_STENCIL};
     gl->InvalidateFramebuffer(GL_FRAMEBUFFER, 2, attachments);
-#else
-    // Since TV driver throw useless gles error when we use GL_STENCIL to framebuffer, let we don't invalidate STENCIL for TV. 2025-02-24 eunkiki.hong@samsung.com
-    GLenum attachments[] = {GL_DEPTH};
-    gl->InvalidateFramebuffer(GL_FRAMEBUFFER, 1, attachments);
-#endif
   }
+#else
+  // Since TV driver throw useless gles error when we use GL_STENCIL and GL_DEPTH to framebuffer, let we don't invalidate framebuffer for TV. 2025-02-24 eunkiki.hong@samsung.com
+#endif
 }
 
 void Context::SetScissorTestEnabled(bool scissorEnabled)
