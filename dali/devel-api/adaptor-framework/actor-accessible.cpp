005efe4f7e1d22f1b65cab47fd44eee728035fb5
Youngsun Suh <youngsun.suh@samsung.com>
2025-06-12 09:01:17 +0900

    Clear a11y events cache on pause
    Change-Id: Ie46e4e316fa8d7e8c26b3b51affa2d68819e6a6a

--- dali/devel-api/adaptor-framework/actor-accessible.cpp
+++ dali/devel-api/adaptor-framework/actor-accessible.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 Samsung Electronics Co., Ltd.
+ * Copyright (c) 2025 Samsung Electronics Co., Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -431,6 +431,11 @@ void ActorAccessible::NotifyAccessibilityStateChange(Dali::Accessibility::States
       }
     }
   }
+}
+
+void ActorAccessible::ClearCache()
+{
+  mLastEmittedState.clear();
 }
 
 } // namespace Dali::Accessibility
