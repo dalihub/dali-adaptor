0b5e9fa44c35ec0bee756b49f334ad1d2446ee8b
huayongxu <huayong.xu@samsung.com>
2025-11-10 16:37:46 +0800

    [Tizen]Add some APIs for file chooser request.
    Change-Id: I62c0b839ec888908a8538385cc30fe99072c42f8

--- dali/internal/web-engine/common/web-engine-impl.h
+++ dali/internal/web-engine/common/web-engine-impl.h
@@ -2,7 +2,7 @@
 #define DALI_WEB_ENGINE_IMPL_H
 
 /*
- * Copyright (c) 2024 Samsung Electronics Co., Ltd.
+ * Copyright (c) 2025 Samsung Electronics Co., Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -621,6 +621,11 @@ public:
    * @copydoc Dali::WebEngine::RegisterWebAuthResponseCallback()
    */
   void RegisterWebAuthResponseCallback(Dali::WebEnginePlugin::WebEngineWebAuthResponseCallback callback);
+
+  /**
+   * @copydoc Dali::WebEngine::RegisterFileChooserRequestedCallback()
+   */
+  void RegisterFileChooserRequestedCallback(Dali::WebEnginePlugin::WebEngineFileChooserRequestedCallback callback);
 
   /**
    * @copydoc Dali::WebEngine::RegisterUserMediaPermissionRequestCallback()
