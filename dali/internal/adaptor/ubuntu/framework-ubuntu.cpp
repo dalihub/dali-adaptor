03f0e8425fac768f2f553111d9f4889139bc5936
huayongxu <huayong.xu@samsung.com>
2026-02-27 11:15:41 +0800

    Add elm_shutdown before app exits on Ubuntu.
    elm_shutdown would free memories used in elm.
    then memory issues information reported by ASAN is clearer.
    
    Change-Id: I87b4e6ff9008ec4f438d706802159b2d4ec4d136

--- dali/internal/adaptor/ubuntu/framework-ubuntu.cpp
+++ dali/internal/adaptor/ubuntu/framework-ubuntu.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2025 Samsung Electronics Co., Ltd.
+ * Copyright (c) 2026 Samsung Electronics Co., Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -55,6 +55,8 @@ void FrameworkUbuntu::Run()
   mObserver.OnInit();
 
   elm_run();
+
+  elm_shutdown();
 
   mRunning = false;
 }
