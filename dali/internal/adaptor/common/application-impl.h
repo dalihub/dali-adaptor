aa8f0509a1f7461820b048060582dd3fa5e76f82
Eunki Hong <eunkiki.hong@samsung.com>
2026-05-11 15:13:57 +0900

    (Application) Quit framework immediatly if Application::Quit() comes.
    Let we quit the framework when application quit requestd, not on idler.
    
    Usually application frameworks also try to quit the application at idle time.
    And if we send to application quited, appfw could ignore some logics if they needs.
    
    Change-Id: Iaec6e35e79e08867646fced352d1943cdd7b2f0c
    Signed-off-by: Eunki Hong <eunkiki.hong@samsung.com>

--- dali/internal/adaptor/common/application-impl.h
+++ dali/internal/adaptor/common/application-impl.h
@@ -443,11 +443,6 @@ protected:
   Application(const Application&);
   Application& operator=(Application&);
 
-  /**
-   * Quits from the main loop
-   */
-  void QuitFromMainLoop();
-
 private:
   AppSignalType                      mInitSignal;
   AppSignalType                      mTerminateSignal;
